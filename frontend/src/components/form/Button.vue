30ab9dfa24948046ea69c692751c5ddf5174572a
lalize <printf@kakao.com>
2020-09-09 15:53:12 +0900

    feat: add button component

---
+++ frontend/src/components/form/Button.vue
@@ -0,0 +1,36 @@
+<template>
+  <input class="button" :class="{ cancel }" />
+</template>
+
+<script>
+export const Button = {
+  props: {
+    cancel: Boolean,
+  },
+}
+export default Button
+</script>
+
+<style scoped>
+.button {
+  cursor: pointer;
+  outline: none;
+  border: 0;
+  background: #0078ff;
+  color: #fff;
+  border-radius: 3px;
+  padding: 10px;
+  min-width: 80px;
+  min-height: 28px;
+  margin: 5px;
+}
+
+.button.cancel {
+  background: #999;
+}
+
+.button:disabled {
+  cursor: default;
+  background: #ccc;
+}
+</style>
