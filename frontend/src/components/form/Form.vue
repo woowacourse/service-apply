afc345fe72f8a7f0444bbdf94e0ea5fbb220a02e
lalize <printf@kakao.com>
2020-09-09 15:55:02 +0900

    feat: add form component

---
+++ frontend/src/components/form/Form.vue
@@ -0,0 +1,34 @@
+<template>
+  <form class="form">
+    <slot></slot>
+  </form>
+</template>
+
+<script>
+export const Form = {}
+export default Form
+</script>
+
+<style scoped>
+.form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 512px;
+  padding: 20px;
+  margin: 15px;
+  border-radius: 3px;
+  background: #f1f2f6;
+  box-shadow: 0 0 7px rgba(0, 0, 0, 0.05);
+}
+
+@media screen and (max-width: 512px) {
+  .form {
+    margin: 0;
+  }
+}
+
+.form > h1 {
+  color: #2c3e50;
+}
+</style>
