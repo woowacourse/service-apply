2efc7ba4f30969f5b69c2b40675867bd96ef7be8
lalize <printf@kakao.com>
2020-09-09 15:50:01 +0900

    feat: add field component

---
+++ frontend/src/components/form/Field.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="field">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export const Field = {}
+export default Field
+</script>
+
+<style scoped>
+.field {
+  display: flex;
+  flex-direction: column;
+  margin: 10px 0;
+}
+</style>
