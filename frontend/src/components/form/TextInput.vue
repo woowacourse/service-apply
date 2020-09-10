c773ae301f0803de3594c6d98848ff98de57997a
Youngho Park <printf@kakao.com>
2020-09-10 13:52:43 +0900

    feat(apply): implement applicant information input form

---
+++ frontend/src/components/form/TextInput.vue
@@ -0,0 +1,47 @@
+<template>
+  <input
+    :type="type"
+    class="text-input"
+    :value="value"
+    @input="$emit('input', $event.target.value)"
+  />
+</template>
+
+<script>
+const TextInput = {
+  props: {
+    type: {
+      type: String,
+      default: "text",
+      validator(value) {
+        return ["text", "email", "password"].indexOf(value) !== -1
+      },
+    },
+    value: String,
+  },
+}
+
+export default TextInput
+</script>
+
+<style scoped>
+input[type="text"],
+input[type="email"],
+input[type="password"] {
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  outline: none;
+}
+
+.text-input {
+  padding: 12px;
+  border: 1px solid #ced6e0;
+  border-radius: 3px;
+  transition-duration: 0.5s;
+}
+
+.text-input:focus {
+  border: 1px solid #1e90ff;
+}
+</style>
---
+++ frontend/src/utils/validation/regist.js
@@ -0,0 +1,9 @@
+import { common } from "./common"
+
+export const regist = {
+  name: [...common.required],
+  phoneNumber: [...common.required],
+  email: [...common.required],
+  password: [...common.required],
+  rePassword: [...common.required],
+}
