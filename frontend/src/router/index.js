09f74870c5c6208f87cfb565ec2e60bdd0d911d4
juyeongjung <juyeong.dev@gmail.com>
2020-09-10 16:58:30 +0900

    chore: rename LoginPage to Login

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,7 +2,7 @@ import Vue from "vue"
 import VueRouter from "vue-router"
 import Regist from "@/views/Regist"
 import RecruitmentRegister from "@/views/RecruitmentRegister"
-import LoginPage from "@/views/LoginPage"
+import Login from "@/views/Login"
 
 Vue.use(VueRouter)
 
@@ -17,8 +17,7 @@ const routes = [
   },
   {
     path: "/login",
-    name: "LoginPage",
-    component: LoginPage,
+    component: Login,
   },
 ]
 
