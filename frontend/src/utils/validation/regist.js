abe84e9fc2527f3da28a1c20253e67a9e9302941
juyeongjung <juyeong.dev@gmail.com>
2020-09-10 18:01:34 +0900

    feat: apply email, name validation in Regist

--- frontend/src/utils/validation/regist.js
+++ frontend/src/utils/validation/regist.js
@@ -1,9 +1,11 @@
 import { common } from "./common"
+import { name } from "./name"
+import { email } from "./email"
 
 export const regist = {
-  name: [...common.required],
+  name: [...name],
   phoneNumber: [...common.required],
-  email: [...common.required],
+  email: [...email],
   password: [...common.required],
   rePassword: [...common.required],
 }
