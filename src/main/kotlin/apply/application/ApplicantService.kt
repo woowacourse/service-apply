3ce2bd6461bb7563e38e6643fbb3ff65476c7393
BEOMJUN KIM <skyblue300a@gmail.com>
2020-09-14 21:52:47 +0900

    feat: implement the logic which generate token with given email

--- src/main/kotlin/apply/application/ApplicantService.kt
+++ src/main/kotlin/apply/application/ApplicantService.kt
@@ -3,20 +3,54 @@ package apply.application
 import apply.domain.applicant.Applicant
 import apply.domain.applicant.ApplicantRepository
 import apply.domain.applicant.Gender
+import apply.security.JwtTokenProvider
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import support.createLocalDate
+import java.time.LocalDate
 import javax.annotation.PostConstruct
 
 @Transactional
 @Service
-class ApplicantService(private val applicantRepository: ApplicantRepository) {
+class ApplicantService(
+    private val applicantRepository: ApplicantRepository,
+    private val jwtTokenProvider: JwtTokenProvider
+) {
     fun findAll(): List<Applicant> {
         return applicantRepository.findAll()
     }
 
     fun findByValue(value: String): List<Applicant> =
         applicantRepository.findByNameContainingOrEmailContaining(value, value)
+
+    fun validateOrCreateApplicantAndGenerateToken(
+        name: String,
+        email: String,
+        phoneNumber: String,
+        gender: Gender,
+        birthDay: LocalDate
+    ): String {
+        applicantRepository.findByEmail(email) ?: createApplicant(name, email, phoneNumber, gender, birthDay)
+
+        return jwtTokenProvider.createToken(email)
+    }
+
+    private fun createApplicant(
+        name: String,
+        email: String,
+        phoneNumber: String,
+        gender: Gender,
+        birthDay: LocalDate
+    ) {
+        val newApplicant = Applicant(
+            name = name,
+            email = email,
+            phoneNumber = phoneNumber,
+            gender = gender,
+            birthday = birthDay
+        )
+        applicantRepository.save(newApplicant)
+    }
 
     @PostConstruct
     private fun populateDummy() {
