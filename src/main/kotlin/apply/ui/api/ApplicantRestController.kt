5ba3fbd404b75d91297f3f904e07ab7f79286d10
BEOMJUN KIM <skyblue300a@gmail.com>
2020-09-15 21:10:19 +0900

    feat: implement unauthorized logic

--- src/main/kotlin/apply/ui/api/ApplicantRestController.kt
+++ src/main/kotlin/apply/ui/api/ApplicantRestController.kt
@@ -2,6 +2,8 @@ package apply.ui.api
 
 import apply.application.ApplicantService
 import apply.domain.applicant.dto.ApplicantRequest
+import apply.domain.applicant.exception.ApplicantValidateException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -15,7 +17,11 @@ class ApplicantRestController(
 ) {
     @PostMapping
     fun generateToken(@RequestBody applicantRequest: ApplicantRequest): ResponseEntity<String> {
-        val token = applicantService.validateOrCreateApplicantAndGenerateToken(applicantRequest)
-        return ResponseEntity.ok().body(token)
+        return try {
+            val token = applicantService.validateOrCreateApplicantAndGenerateToken(applicantRequest)
+            ResponseEntity.ok().body(token)
+        } catch (e: ApplicantValidateException) {
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(e.message)
+        }
     }
 }
