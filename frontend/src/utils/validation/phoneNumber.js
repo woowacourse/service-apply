ed0c5a9cacb9a151c3a836cfcb645d44e4d87461
juyeongjung <juyeong.dev@gmail.com>
2020-09-15 14:23:51 +0900

    chore: change phoneNumber valid message

--- frontend/src/utils/validation/phoneNumber.js
+++ frontend/src/utils/validation/phoneNumber.js
@@ -2,4 +2,4 @@ import { common } from "./common"
 
 const REGEX = /^\d{2,3}-\d{3,4}-\d{4}$/
 
-export const phoneNumber = [...common.required, v => REGEX.test(v) || "정확한 전화번호를 입력해 주세요."]
+export const phoneNumber = [...common.required, v => REGEX.test(v) || "정확한 전화번호를 입력해 주세요. ex) 010-1234-5678"]
