2e04e0e34127b5cd4e68ce572f4d5ef99d9c7e04
juyeongjung <juyeong.dev@gmail.com>
2020-09-15 14:15:02 +0900

    feat: add phoneNumber validation

--- frontend/src/utils/validation/register.js
+++ frontend/src/utils/validation/register.js
@@ -1,10 +1,11 @@
 import { common } from "./common"
 import { name } from "./name"
 import { email } from "./email"
+import { phoneNumber } from "./phoneNumber"
 
 export const register = {
   name: [...name],
-  phoneNumber: [...common.required],
+  phoneNumber: [...phoneNumber],
   email: [...email],
   password: [...common.required],
   rePassword: [...common.required],
