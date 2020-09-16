35eecdc61cd6816fccacca1ac4f7bb3614bd43f7
toneyparky <qkrtmddhks95@gmail.com>
2020-09-16 12:40:56 +0900

    feat(evaluation item): implement EvaluationRestController
    - change findAll() to public

---
+++ src/main/kotlin/apply/ui/api/EvaluationRestController.kt
@@ -0,0 +1,28 @@
+package apply.ui.api
+
+import apply.application.EvaluationItemService
+import apply.application.EvaluationService
+import apply.domain.evaluation.Evaluation
+import apply.domain.evaluationItem.EvaluationItem
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/evaluations")
+class EvaluationRestController(
+    private val evaluationService: EvaluationService,
+    private val evaluationItemService: EvaluationItemService
+) {
+    @GetMapping
+    fun findAll(): ResponseEntity<List<Evaluation>> {
+        return ResponseEntity.ok(evaluationService.findAll())
+    }
+
+    @GetMapping("/{id}/items")
+    fun findItemsByID(@PathVariable("id") evaluationId: Long): ResponseEntity<List<EvaluationItem>> {
+        return ResponseEntity.ok(evaluationItemService.findByEvaluationId(evaluationId))
+    }
+}
