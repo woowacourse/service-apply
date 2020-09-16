2674aee23f2502af70aefae653c6fe4b978d5ec2
BEOMJUN KIM <skyblue300a@gmail.com>
2020-09-16 22:06:42 +0900

    refactor: add applicant service test

---
+++ src/test/kotlin/apply/application/ApplicantServiceTest.kt
@@ -0,0 +1,92 @@
+package apply.application
+
+import apply.domain.applicant.Applicant
+import apply.domain.applicant.ApplicantRepository
+import apply.domain.applicant.Gender
+import apply.domain.applicant.dto.ApplicantRequest
+import apply.domain.applicant.exception.ApplicantValidateException
+import apply.security.JwtTokenProvider
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.ArgumentMatchers
+import org.mockito.BDDMockito.given
+import org.mockito.Mock
+import org.mockito.Mockito.verify
+import org.mockito.junit.jupiter.MockitoExtension
+import support.createLocalDate
+
+private const val VALID_TOKEN = "SOME_VALID_TOKEN"
+
+@ExtendWith(MockitoExtension::class)
+internal class ApplicantServiceTest {
+    @Mock
+    private lateinit var applicantRepository: ApplicantRepository
+
+    @Mock
+    private lateinit var jwtTokenProvider: JwtTokenProvider
+
+    private lateinit var applicantService: ApplicantService
+
+    private val applicant = Applicant(
+        id = 1L,
+        name = "지원자",
+        email = "test@email.com",
+        phoneNumber = "010-0000-0000",
+        gender = Gender.MALE,
+        birthday = createLocalDate(1995, 2, 2),
+        password = "password"
+    )
+    private val validApplicantRequest = ApplicantRequest(
+        name = "지원자",
+        email = "test@email.com",
+        phoneNumber = "010-0000-0000",
+        gender = Gender.MALE,
+        birthday = createLocalDate(1995, 2, 2),
+        password = "password"
+    )
+    private val inValidApplicantRequest = ApplicantRequest(
+        name = "지원자",
+        email = "test@email.com",
+        phoneNumber = "010-0000-0000",
+        gender = Gender.MALE,
+        birthday = createLocalDate(1995, 2, 2),
+        password = "invalid_password"
+    )
+
+    @BeforeEach
+    internal fun setUp() {
+        applicantService = ApplicantService(applicantRepository, jwtTokenProvider)
+    }
+
+    @Test
+    fun `지원자가 이미 존재하고 검증에 성공하면 유효한 토큰을 반환한다`() {
+        given(applicantRepository.findByEmail(validApplicantRequest.email)).willReturn(applicant)
+        given(jwtTokenProvider.createToken(validApplicantRequest.email)).willReturn(VALID_TOKEN)
+
+        assertThat(applicantService.validateOrCreateApplicantAndGenerateToken(1L, validApplicantRequest))
+            .isEqualTo(VALID_TOKEN)
+    }
+
+    @Test
+    fun `지원자가 이미 존재하고 필드 값 동등성 검증에 실패하면 ApplicantValidateException이 발생한다`() {
+        given(applicantRepository.findByEmail(inValidApplicantRequest.email)).willReturn(applicant)
+
+        assertThatThrownBy {
+            applicantService.validateOrCreateApplicantAndGenerateToken(1L, inValidApplicantRequest)
+        }.isInstanceOf(ApplicantValidateException::class.java)
+            .hasMessage("비밀번호 값이 기존 정보와 일치하지 않습니다")
+    }
+
+    @Test
+    fun `지원자가 존재하지 않으면 지원자를 저장한 뒤, 유효한 토큰을 반환한다`() {
+        given(applicantRepository.findByEmail(validApplicantRequest.email)).willReturn(null)
+        given(jwtTokenProvider.createToken(validApplicantRequest.email)).willReturn(VALID_TOKEN)
+
+        assertThat(applicantService.validateOrCreateApplicantAndGenerateToken(1L, validApplicantRequest))
+            .isEqualTo(VALID_TOKEN)
+        verify(applicantRepository).save(ArgumentMatchers.refEq(applicant, "id"))
+    }
+}
