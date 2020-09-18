e650344bde3903d080670632452434cb41360b75
woowahan-pjs <jason@woowahan.com>
2020-09-18 21:44:17 +0900

    fix(recruitment): add lob annotation

--- src/main/kotlin/apply/domain/recruitmentitem/RecruitmentItem.kt
+++ src/main/kotlin/apply/domain/recruitmentitem/RecruitmentItem.kt
@@ -5,6 +5,7 @@ import javax.persistence.Entity
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
+import javax.persistence.Lob
 
 @Entity
 class RecruitmentItem(
@@ -21,6 +22,7 @@ class RecruitmentItem(
     val maximumLength: Int,
 
     @Column(nullable = false)
+    @Lob
     val description: String,
 
     @Id
