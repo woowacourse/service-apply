3f6446e69da696cdee3cd5b00d7979bf80f9d607
lowoon <sangbo0229@gmail.com>
2020-09-18 12:48:57 +0900

    feat: apply lob to answer contents

--- src/main/kotlin/apply/domain/recruitmentitem/Answer.kt
+++ src/main/kotlin/apply/domain/recruitmentitem/Answer.kt
@@ -2,9 +2,11 @@ package apply.domain.recruitmentitem
 
 import javax.persistence.Column
 import javax.persistence.Embeddable
+import javax.persistence.Lob
 
 @Embeddable
 class Answer(
+    @Lob
     val contents: String,
 
     @Column(nullable = false)
