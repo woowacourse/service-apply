9c18f62dac725b094ec71a8de58c0155c6748104
BEOMJUN KIM <skyblue300a@gmail.com>
2020-09-18 13:10:57 +0900

    refactor: modify token validity time to negative

--- src/test/kotlin/apply/security/JwtTokenProviderTest.kt
+++ src/test/kotlin/apply/security/JwtTokenProviderTest.kt
@@ -3,12 +3,11 @@ package apply.security
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
-internal class JwtTokenProviderTest {
-    companion object {
-        private const val PAYLOAD = "email@email.com"
-        private const val NOT_VALID_TOKEN = "NOT_VALID_TOKEN"
-    }
+private const val PAYLOAD = "email@email.com"
+private const val NOT_VALID_TOKEN = "NOT_VALID_TOKEN"
+private const val NEGATIVE_VALIDITY_TIME = -10L
 
+internal class JwtTokenProviderTest {
     @Test
     fun `payload를 넣어 토큰을 생성하고 토큰에서 다시 payload를 불러올 수 있는지 확인한다`() {
         val jwtTokenProvider = JwtTokenProvider()
@@ -19,7 +18,7 @@ internal class JwtTokenProviderTest {
 
     @Test
     fun `유효시간이 지난 토큰의 유효성 검사가 실패한다`() {
-        val jwtTokenProvider = JwtTokenProvider(validityInMilliseconds = 10)
+        val jwtTokenProvider = JwtTokenProvider(validityInMilliseconds = NEGATIVE_VALIDITY_TIME)
         val token = jwtTokenProvider.createToken(PAYLOAD)
 
         assertThat(jwtTokenProvider.validateToken(token)).isFalse()
