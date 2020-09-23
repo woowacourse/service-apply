3f446f413657553d548dee8a969d5529f4a33ed0
Andy H. Jung <commaniakr@gmail.com>
2020-09-24 01:35:44 +0900

    fix: fixed git conflicts about ApplicationForm / ApplicationFormRepository

--- src/main/kotlin/apply/domain/applicationform/ApplicationForm.kt
+++ src/main/kotlin/apply/domain/applicationform/ApplicationForm.kt
@@ -1,32 +1,28 @@
-package apply.domain.applicationform
+package apply.domain.applicationForm
 
-import apply.domain.recruitmentitem.Answers
 import java.time.LocalDateTime
 import javax.persistence.*
 
 @Entity
 class ApplicationForm(
         @Column(nullable = false)
-        var applicantId: Long,
+        val applicantId: Long,
 
         @Column(nullable = false)
-        var recruitmentId: Long,
+        val recruitmentId: Long,
 
+        @Column
         var referenceUrl: String,
-
-        @Embedded
-        var answers: Answers,
 
         @Id
         @GeneratedValue(strategy = GenerationType.IDENTITY)
         val id: Long = 0L
 ) {
-
     @Column(nullable = false)
     var submitted: Boolean = false
 
     @Column(nullable = false)
-    var createdDateTime: LocalDateTime = LocalDateTime.now()
+    val createdDateTime: LocalDateTime = LocalDateTime.now()
 
     @Column(nullable = false)
     var modifiedDateTime: LocalDateTime = LocalDateTime.now()
@@ -34,27 +30,12 @@ class ApplicationForm(
     @Column
     var submittedDateTime: LocalDateTime? = null
 
-    constructor(
-            applicantId: Long,
-            recruitmentId: Long,
-            referenceUrl: String,
-            answers: Answers,
-            submitted: Boolean,
-            createdDateTime: LocalDateTime,
-            modifiedDateTime: LocalDateTime,
-            submittedDateTime: LocalDateTime):this(applicantId, recruitmentId, referenceUrl, answers) {
-                this.submitted = submitted
-                this.createdDateTime = createdDateTime
-                this.modifiedDateTime = modifiedDateTime
-                this.submittedDateTime = submittedDateTime
-    }
-    fun update(referenceUrl: String, answers: Answers) {
+    fun update(referenceUrl: String) {
         if (this.submitted) {
             throw IllegalAccessException("이미 제출된 지원서입니다. 수정할 수 없습니다.")
         }
         this.referenceUrl = referenceUrl
         this.modifiedDateTime = LocalDateTime.now()
-        this.answers = answers
     }
 
     fun submit() {
--- src/main/kotlin/apply/domain/applicationform/ApplicationFormRepository.kt
+++ src/main/kotlin/apply/domain/applicationform/ApplicationFormRepository.kt
@@ -1,9 +1,7 @@
-package apply.domain.applicationform
+package apply.domain.applicationForm
 
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface ApplicationFormRepository : JpaRepository<ApplicationForm, Long> {
-    fun findByRecruitmentId(recruitmentId: Long): List<ApplicationForm>
-
-    fun findByRecruitmentIdAndApplicantId(recruitmentId: Long, applicantId: Long): ApplicationForm?
+    fun findByApplicantIdAndRecruitmentId(applicantId: Long, recruitmentId: Long) : ApplicationForm?
 }
