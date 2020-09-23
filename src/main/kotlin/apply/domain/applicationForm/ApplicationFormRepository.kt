ee81cd522aaedcb0e55745aecae51389d1227df3
Andy H. Jung <commaniakr@gmail.com>
2020-09-24 01:42:00 +0900

    fix: temporary deleting ApplicationForm / ApplicationFormRepository

--- src/main/kotlin/apply/domain/applicationForm/ApplicationFormRepository.kt
+++
@@ -1,7 +0,0 @@
-package apply.domain.applicationForm
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface ApplicationFormRepository : JpaRepository<ApplicationForm, Long> {
-    fun findByApplicantIdAndRecruitmentId(applicantId: Long, recruitmentId: Long) : ApplicationForm?
-}
