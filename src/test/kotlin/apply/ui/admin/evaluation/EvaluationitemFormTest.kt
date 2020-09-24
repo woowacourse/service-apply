fa1235b424bde810f0d18319308150077b2264f6
lowoon <sangbo0229@gmail.com>
2020-09-24 14:29:43 +0900

    refactor: change fixture data

--- src/test/kotlin/apply/ui/admin/evaluation/EvaluationitemFormTest.kt
+++ src/test/kotlin/apply/ui/admin/evaluation/EvaluationitemFormTest.kt
@@ -5,10 +5,11 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertAll
 
 private fun createEvaluationItemForm(
-    title: String = "프로그래밍 학습 과정과 현재 자신이 생각하는 역량은?",
+    title: String = "README.md 파일에 기능 목록이 추가되어 있는가?",
     maximumScore: Int = 2,
     position: Int = 1,
-    description: String = "우아한테크코스는 프로그래밍에 대한 기본 지식과 경험을 가진 교육생을 선발하기 때문에 프로그래밍 경험이 있는 상태에서 지원하게 됩니다. 프로그래밍 학습을 어떤 계기로 시작했으며, 어떻게 학습해왔는지, 이를 통해 현재 어느 정도의 역량을 보유한 상태인지를 구체적으로 작성해 주세요."
+    description: String = "[리뷰 절차]\n" +
+        "https://github.com/woowacourse/woowacourse-docs/tree/master/precourse"
 ): EvaluationItemForm {
     return EvaluationItemForm(title, maximumScore, position, description)
 }
@@ -19,7 +20,7 @@ internal class EvaluationItemFormTest {
         val actual = createEvaluationItemForm().bindOrNull()
         assertThat(actual).isNotNull
         assertAll(
-            { assertThat(actual!!.title).isEqualTo("프로그래밍 학습 과정과 현재 자신이 생각하는 역량은?") },
+            { assertThat(actual!!.title).isEqualTo("README.md 파일에 기능 목록이 추가되어 있는가?") },
             { assertThat(actual!!.position).isEqualTo(1) },
             { assertThat(actual!!.maximumScore).isEqualTo(2) }
         )
