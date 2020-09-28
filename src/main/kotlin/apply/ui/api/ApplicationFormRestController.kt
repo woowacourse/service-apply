8fc181cd2b2c8843e03f6cb80d86aad533467869
Andy H. Jung <commaniakr@gmail.com>
2020-09-28 17:30:57 +0900

    refactor: ApplicationFormController -> ApplicationFormRestController.kt
    - changed request URI into "api/register/applications"

--- src/main/kotlin/apply/ui/api/ApplicationFormRestController.kt
+++ src/main/kotlin/apply/ui/api/ApplicationFormRestController.kt
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/api/register/application")
-class ApplicationFormController(
+@RequestMapping("/api/register/applications")
+class ApplicationFormRestController(
     private val applicationFormService: ApplicationFormService
 ) {
     @GetMapping("/{id}")
