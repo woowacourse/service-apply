64a151018ee6e9b6296dcc806a32c1c7c5482626
BEOMJUN KIM <skyblue300a@gmail.com>
2020-10-01 11:50:25 +0900

    refactor: modify support logic of the BearerAuthInterceptor

--- src/main/kotlin/apply/security/BearerAuthInterceptor.kt
+++ src/main/kotlin/apply/security/BearerAuthInterceptor.kt
@@ -1,15 +1,19 @@
 package apply.security
 
+import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.web.method.HandlerMethod
 import org.springframework.web.servlet.HandlerInterceptor
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
+import kotlin.reflect.KClass
 
 private const val AUTHORIZATION_HEADER = "Authorization"
 private const val BEARER = "Bearer"
 
 const val APPLICANT_EMAIL_ATTRIBUTE_NAME = "loginApplicantEmail"
+
+private val SUPPORTED_PARAMETER_ANNOTATIONS: List<KClass<out Annotation>> = listOf(LoginApplicant::class)
 
 @Component
 class BearerAuthInterceptor(private val jwtTokenProvider: JwtTokenProvider) : HandlerInterceptor {
@@ -17,7 +21,7 @@ class BearerAuthInterceptor(private val jwtTokenProvider: JwtTokenProvider) : Ha
     override fun preHandle(request: HttpServletRequest, response: HttpServletResponse, handler: Any): Boolean {
         val handlerMethod = handler as? HandlerMethod ?: return true
 
-        if (handlerMethod.methodParameters.none { it.hasParameterAnnotation(LoginApplicant::class.java) }) {
+        if (!hasSupportedParameterAnnotations(handlerMethod.methodParameters)) {
             return true
         }
 
@@ -26,6 +30,14 @@ class BearerAuthInterceptor(private val jwtTokenProvider: JwtTokenProvider) : Ha
         request.setAttribute(APPLICANT_EMAIL_ATTRIBUTE_NAME, applicantEmail)
 
         return true
+    }
+
+    private fun hasSupportedParameterAnnotations(methodParameters: Array<MethodParameter>): Boolean {
+        return methodParameters.any { methodParameter ->
+            methodParameter.parameterAnnotations.any {
+                SUPPORTED_PARAMETER_ANNOTATIONS.contains(it.annotationClass)
+            }
+        }
     }
 
     private fun extractBearerToken(request: HttpServletRequest): String {
