5ec033b84641a4eadee6e59a9118508a932a00e3
juyeongjung <juyeong.dev@gmail.com>
2020-10-03 17:55:42 +0900

    feat: enable async on sendEmail

--- src/main/kotlin/apply/Application.kt
+++ src/main/kotlin/apply/Application.kt
@@ -2,7 +2,9 @@ package apply
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @SpringBootApplication
 class Application
 
--- src/main/kotlin/apply/application/MailService.kt
+++ src/main/kotlin/apply/application/MailService.kt
@@ -3,6 +3,7 @@ package apply.application
 import apply.domain.applicant.ApplicantPasswordFindInformation
 import org.springframework.mail.SimpleMailMessage
 import org.springframework.mail.javamail.JavaMailSender
+import org.springframework.scheduling.annotation.Async
 import org.springframework.stereotype.Service
 
 @Service
@@ -10,6 +11,7 @@ class MailService(
     private val mailSender: JavaMailSender,
     private val message: SimpleMailMessage
 ) {
+    @Async
     fun sendPasswordResetMail(applicantPasswordFindInformation: ApplicantPasswordFindInformation, newPassword: String) {
         message.setTo(applicantPasswordFindInformation.email)
         message.setSubject("${applicantPasswordFindInformation.name}님, 임시 비밀번호를 발송해 드립니다.")
