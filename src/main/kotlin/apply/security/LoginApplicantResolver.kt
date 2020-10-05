059c4b30599363be94f1b5c009aef9f045c16c25
BEOMJUN KIM <skyblue300a@gmail.com>
2020-10-05 23:08:43 +0900

    refactor: remove BearerAuthInterceptor and move its logic to LoginApplicantResolver

--- src/main/kotlin/apply/security/LoginApplicantResolver.kt
+++ src/main/kotlin/apply/security/LoginApplicantResolver.kt
@@ -6,12 +6,17 @@ import org.springframework.core.MethodParameter
 import org.springframework.stereotype.Component
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 
+const val AUTHORIZATION_HEADER = "Authorization"
+private const val BEARER = "Bearer"
+
 @Component
-class LoginApplicantResolver(private val applicantService: ApplicantService) : HandlerMethodArgumentResolver {
+class LoginApplicantResolver(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val applicantService: ApplicantService
+) : HandlerMethodArgumentResolver {
 
     override fun supportsParameter(parameter: MethodParameter) =
         parameter.hasParameterAnnotation(LoginApplicant::class.java)
@@ -22,9 +27,32 @@ class LoginApplicantResolver(private val applicantService: ApplicantService) : H
         webRequest: NativeWebRequest,
         binderFactory: WebDataBinderFactory?
     ): Applicant {
-        val applicantEmail = webRequest.getAttribute(APPLICANT_EMAIL_ATTRIBUTE_NAME, SCOPE_REQUEST) as? String
-            ?: throw AssertionError("요청에 loginApplicantEmail 속성이 반드시 존재해야 합니다")
+        val token = extractBearerToken(webRequest)
+        if (!jwtTokenProvider.isValidToken(token)) {
+            throw IllegalArgumentException("로그인 정보가 정확하지 않습니다")
+        }
+        val applicantEmail = jwtTokenProvider.getSubject(token)
 
         return applicantService.getByEmail(applicantEmail)
+    }
+
+    private fun extractBearerToken(request: NativeWebRequest): String {
+        val authorization = request.getHeader(AUTHORIZATION_HEADER)
+            ?: throw IllegalArgumentException("로그인 정보가 정확하지 않습니다")
+
+        val (tokenType, token) = splitToTokenFormat(authorization)
+        if (tokenType != BEARER) {
+            throw IllegalArgumentException("로그인 정보가 정확하지 않습니다")
+        }
+        return token
+    }
+
+    private fun splitToTokenFormat(authorization: String): Pair<String, String> {
+        return try {
+            val tokenFormat = authorization.split(" ")
+            Pair(tokenFormat[0], tokenFormat[1])
+        } catch (e: IndexOutOfBoundsException) {
+            throw IllegalArgumentException("로그인 정보가 정확하지 않습니다")
+        }
     }
 }
--- src/test/kotlin/apply/security/LoginApplicantResolverTest.kt
+++ src/test/kotlin/apply/security/LoginApplicantResolverTest.kt
@@ -8,44 +8,54 @@ import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
 import io.mockk.mockk
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 import org.springframework.core.MethodParameter
 import org.springframework.web.context.request.NativeWebRequest
-import org.springframework.web.context.request.RequestAttributes.SCOPE_REQUEST
 import support.createLocalDate
 
 @ExtendWith(MockKExtension::class)
 internal class LoginApplicantResolverTest {
-
     @MockK
     private lateinit var applicantService: ApplicantService
 
+    @MockK
+    private lateinit var jwtTokenProvider: JwtTokenProvider
     private lateinit var loginApplicantResolver: LoginApplicantResolver
-
-    private lateinit var loginApplicantParameter: MethodParameter
+    private lateinit var methodParameter: MethodParameter
+    private lateinit var nativeWebRequest: NativeWebRequest
 
     @BeforeEach
     internal fun setUp() {
-        loginApplicantResolver = LoginApplicantResolver(applicantService)
+        loginApplicantResolver = LoginApplicantResolver(jwtTokenProvider, applicantService)
+        methodParameter = mockk()
+        nativeWebRequest = mockk()
+    }
 
-        val method = TestAuthController::class.java.getDeclaredMethod("authTargetMethod", Applicant::class.java)
-        loginApplicantParameter = MethodParameter.forExecutable(method, 0)
+    @ParameterizedTest
+    @CsvSource("authTargetMethod,true", "nonAuthTargetMethod,false")
+    fun `@LoginApplicant 주어진 메서드의 서포트 여부를 확인한다`(methodName: String, expected: Boolean) {
+        val method = TestAuthController::class.java.getDeclaredMethod(methodName, Applicant::class.java)
+        val loginApplicantParameter: MethodParameter = MethodParameter.forExecutable(method, 0)
+
+        assertThat(loginApplicantResolver.supportsParameter(loginApplicantParameter)).isEqualTo(expected)
+    }
+
+    private class TestAuthController {
+        fun authTargetMethod(@LoginApplicant applicant: Applicant) {}
+
+        fun nonAuthTargetMethod(applicant: Applicant) {}
     }
 
     @Test
-    fun `@LoginApplicant 어노테이션을 서포트하는지 확인한다`() {
-        assertThat(loginApplicantResolver.supportsParameter(loginApplicantParameter)).isTrue()
-    }
-
-    @Test
-    fun `요청에 담긴 loginApplicantEmail 속성으로 저장된 지원자를 불러온다`() {
-        val nativeWebRequest = mockk<NativeWebRequest>()
-        every {
-            nativeWebRequest.getAttribute(APPLICANT_EMAIL_ATTRIBUTE_NAME, SCOPE_REQUEST)
-        } returns "applicant_email@email.com"
-
+    fun `요청의 Authorization 헤더로 저장된 지원자를 불러온다`() {
+        every { nativeWebRequest.getHeader(AUTHORIZATION_HEADER) } returns "Bearer valid_token"
+        every { jwtTokenProvider.isValidToken("valid_token") } returns true
+        every { jwtTokenProvider.getSubject("valid_token") } returns "applicant_email@email.com"
         val expectedApplicant = Applicant(
             name = "홍길동1",
             email = "applicant_email@email.com",
@@ -56,8 +66,40 @@ internal class LoginApplicantResolverTest {
         )
         every { applicantService.getByEmail("applicant_email@email.com") } returns expectedApplicant
 
-        val result =
-            loginApplicantResolver.resolveArgument(loginApplicantParameter, null, nativeWebRequest, null)
+        val result = loginApplicantResolver.resolveArgument(methodParameter, null, nativeWebRequest, null)
         assertThat(result).usingRecursiveComparison().isEqualTo(expectedApplicant)
+    }
+
+    @ParameterizedTest
+    @CsvSource(
+        "Bearertokeninfo",
+        "''",
+        "Bearer"
+    )
+    fun `요청의 Authorization 헤더의 형식이 올바르지 않을 경우 예외가 발생한다`(header: String) {
+        every { nativeWebRequest.getHeader(AUTHORIZATION_HEADER) } returns header
+
+        assertThatIllegalArgumentException().isThrownBy {
+            loginApplicantResolver.resolveArgument(methodParameter, null, nativeWebRequest, null)
+        }.withMessage("로그인 정보가 정확하지 않습니다")
+    }
+
+    @Test
+    fun `요청의 Authorization 헤더가 존재하지 않을 경우 예외가 발생한다`() {
+        every { nativeWebRequest.getHeader(AUTHORIZATION_HEADER) } returns null
+
+        assertThatIllegalArgumentException().isThrownBy {
+            loginApplicantResolver.resolveArgument(methodParameter, null, nativeWebRequest, null)
+        }.withMessage("로그인 정보가 정확하지 않습니다")
+    }
+
+    @Test
+    fun `요청의 토큰이 유효하지 않은 경우 예외가 발생한다`() {
+        every { nativeWebRequest.getHeader(AUTHORIZATION_HEADER) } returns "invalid_token"
+        every { jwtTokenProvider.isValidToken("invalid_token") } returns false
+
+        assertThatIllegalArgumentException().isThrownBy {
+            loginApplicantResolver.resolveArgument(methodParameter, null, nativeWebRequest, null)
+        }.withMessage("로그인 정보가 정확하지 않습니다")
     }
 }
