36cb0b4590db367be9d1417981b96540575d96f8
Jung JuYeong <10100aaaaa@gmail.com>
2020-10-05 17:21:18 +0900

    feat(apply): implement password finder api and email sender

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,6 +22,7 @@ extra["vaadinVersion"] = "14.3.3"
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-mail")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-web")
---
+++ src/main/kotlin/apply/application/MailService.kt
@@ -0,0 +1,25 @@
+package apply.application
+
+import apply.domain.applicant.ResetPasswordRequest
+import org.springframework.boot.autoconfigure.mail.MailProperties
+import org.springframework.mail.MailSender
+import org.springframework.mail.SimpleMailMessage
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Service
+
+@Service
+class MailService(
+    private val mailSender: MailSender,
+    private val mailProperties: MailProperties
+) {
+    @Async
+    fun sendPasswordResetMail(resetPasswordRequest: ResetPasswordRequest, newPassword: String) {
+        val message = SimpleMailMessage()
+        message.setFrom(mailProperties.username)
+        message.setTo(resetPasswordRequest.email)
+        message.setSubject("${resetPasswordRequest.name}님, 임시 비밀번호를 발송해 드립니다.")
+        message.setText(newPassword)
+
+        mailSender.send(message)
+    }
+}
