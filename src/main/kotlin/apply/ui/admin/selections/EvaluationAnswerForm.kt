9a744be1a20b8348c07f756f30ecc40fa9786a83
BEOMJUN KIM <skyblue300a@gmail.com>
2020-10-05 22:13:13 +0900

    refactor: modify component type of the evaluation item description to tooltip

--- src/main/kotlin/apply/ui/admin/selections/EvaluationAnswerForm.kt
+++ src/main/kotlin/apply/ui/admin/selections/EvaluationAnswerForm.kt
@@ -2,28 +2,23 @@ package apply.ui.admin.selections
 
 import apply.application.EvaluationAnswerData
 import com.vaadin.flow.component.select.Select
-import com.vaadin.flow.component.textfield.TextArea
 import com.vaadin.flow.component.textfield.TextField
+import dev.mett.vaadin.tooltip.Tooltips
 import support.views.BindingIdentityFormLayout
 
 class EvaluationAnswerForm() : BindingIdentityFormLayout<EvaluationAnswerData>(EvaluationAnswerData::class) {
     val score: Select<Int> = Select(0)
-    private val title: TextField = TextField().apply {
-        isReadOnly = true
-        element.style.set("fontWeight", "bold")
-    }
-    private val description: TextArea = TextArea().apply { isReadOnly = true }
+    private val title: TextField = TextField().apply { isReadOnly = true }
 
     init {
-        add(title, score, description)
-        setColspan(description, 2)
+        add(title, score)
         drawRequired()
     }
 
     constructor(title: String, description: String, maximumScore: Int) : this() {
         this.score.setItems((0..maximumScore).toList())
         this.title.value = title
-        this.description.value = description
+        Tooltips.getCurrent().setTooltip(this.title, description)
     }
 
     fun setScoreChangeEvent(scoreChangeEvent: () -> Unit) {
