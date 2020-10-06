caa691d689d8bd17829d8d37d1c6d0de6f9348f2
Youngho Park <printf@kakao.com>
2020-10-06 13:39:11 +0900

    feat(apply): implement the recruits view

--- frontend/src/components/ApplicationFormItem.vue
+++ frontend/src/components/ApplicationFormItem.vue
@@ -1,32 +1,21 @@
 <template>
-  <div class="list-wrapper card">
-    <div class="flex space-between">
-      <div>
-        <div class="recruit-title">
-          <b>{{ recruitment.title }}</b>
-        </div>
-        <div class="recruit-duration">{{ startTime }} ~ {{ endTime }}</div>
-      </div>
-      <div class="button-wrapper">
-        <Button
-          class="enroll-button button"
-          @click="onClickAdmission(recruitment.id)"
-          :disabled="!submittable"
-          :value="buttonLabel"
-        >
-        </Button>
-      </div>
-    </div>
-  </div>
+  <CommonItem
+    :title="recruitment.title"
+    :start-date-time="startDateTime"
+    :end-date-time="endDateTime"
+    :buttonLabel="buttonLabel"
+    :activeButton="submittable"
+    @click="onClickAdmission(recruitment.id)"
+  />
 </template>
 
 <script>
-import Button from "@/components/form/Button"
+import CommonItem from "@/components/CommonItem"
 import { parseLocalDateTime } from "@/utils/date"
 
 export default {
   components: {
-    Button,
+    CommonItem,
   },
   props: {
     recruitment: {
@@ -48,11 +37,11 @@ export default {
     submittable() {
       return !this.submitted && this.recruitment.recruitmentStatus === "RECRUITING"
     },
-    startTime() {
-      return parseLocalDateTime(this.recruitment.startTime)
+    startDateTime() {
+      return parseLocalDateTime(new Date(this.recruitment.startDateTime))
     },
-    endTime() {
-      return parseLocalDateTime(this.recruitment.endTime)
+    endDateTime() {
+      return parseLocalDateTime(new Date(this.recruitment.endDateTime))
     },
   },
   methods: {
@@ -62,65 +51,3 @@ export default {
   },
 }
 </script>
-
-<style>
-@media (max-width: 440px) {
-  .card {
-    height: 90px !important;
-  }
-
-  .button-wrapper {
-    line-height: 90px !important;
-  }
-
-  .list-wrapper {
-    height: 90px !important;
-  }
-}
-
-.card {
-  width: 100%;
-  height: 70px;
-  background-color: #ffffff;
-  border-radius: 3px;
-  display: inline-block;
-  margin: 5px 0 5px 0;
-  box-shadow: 0 0 7px rgba(0, 0, 0, 0.05);
-}
-
-.recruit-title {
-  padding: 10px 0px 5px 10px;
-  font-size: large;
-}
-
-.recruit-duration {
-  padding: 5px 0px 10px 10px;
-}
-
-.flex {
-  display: flex;
-  width: 100%;
-  align-items: center;
-  justify-content: center;
-}
-
-.list-wrapper {
-  display: flex;
-  height: 70px;
-}
-
-.space-between {
-  justify-content: space-between;
-}
-
-.enroll-button {
-  width: 120px;
-  height: 40px;
-  margin: 10px;
-  vertical-align: middle;
-}
-
-.button-wrapper {
-  margin-right: 12px;
-}
-</style>
---
+++ frontend/src/components/Box.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="box">
+    <slot></slot>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  max-width: 800px;
+  padding: 20px;
+  margin: 15px;
+  border-radius: 3px;
+  background: #f1f2f6;
+  box-shadow: 0 0 7px rgba(0, 0, 0, 0.05);
+}
+
+@media screen and (max-width: 800px) {
+  .box {
+    margin: 0;
+  }
+}
+
+.box > h1 {
+  color: #2c3e50;
+}
+</style>
