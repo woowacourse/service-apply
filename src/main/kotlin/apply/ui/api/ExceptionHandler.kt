247d8264575d2bdbfaa565ec389625b9b85ab9dc
lowoon <sangbo0229@gmail.com>
2020-10-06 18:27:08 +0900

    feat: illegal argument exception handler and applicant validate exception handler

--- src/main/kotlin/apply/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/apply/ui/api/ExceptionHandler.kt
@@ -1,5 +1,6 @@
 package apply.ui.api
 
+import apply.domain.applicant.exception.ApplicantValidateException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
@@ -8,6 +9,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 @ControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
+    @ExceptionHandler(value = [ApplicantValidateException::class])
+    fun handleApplicantValidateException(exception: ApplicantValidateException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(exception.message)
+    }
+
+    @ExceptionHandler(value = [IllegalArgumentException::class])
+    fun handleIllegalArgumentException(exception: IllegalArgumentException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(exception.message)
+    }
+
     @ExceptionHandler(value = [Exception::class])
     fun handleGlobalException(exception: Exception): ResponseEntity<String> {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(exception.message)
