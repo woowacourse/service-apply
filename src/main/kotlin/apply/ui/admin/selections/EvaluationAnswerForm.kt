8037ddc440d885007a2ed90a6418e0199d639439
BEOMJUN KIM <skyblue300a@gmail.com>
2020-10-06 10:27:53 +0900

    refactor: modify tooltip to be interactive

--- src/main/kotlin/apply/ui/admin/selections/EvaluationAnswerForm.kt
+++ src/main/kotlin/apply/ui/admin/selections/EvaluationAnswerForm.kt
@@ -4,6 +4,7 @@ import apply.application.EvaluationAnswerData
 import com.vaadin.flow.component.select.Select
 import com.vaadin.flow.component.textfield.TextField
 import dev.mett.vaadin.tooltip.Tooltips
+import dev.mett.vaadin.tooltip.config.TooltipConfiguration
 import support.views.BindingIdentityFormLayout
 
 class EvaluationAnswerForm() : BindingIdentityFormLayout<EvaluationAnswerData>(EvaluationAnswerData::class) {
@@ -18,7 +19,8 @@ class EvaluationAnswerForm() : BindingIdentityFormLayout<EvaluationAnswerData>(E
     constructor(title: String, description: String, maximumScore: Int) : this() {
         this.score.setItems((0..maximumScore).toList())
         this.title.value = title
-        Tooltips.getCurrent().setTooltip(this.title, description)
+        val descriptionTooltip = TooltipConfiguration(description).apply { interactive = true }
+        Tooltips.getCurrent().setTooltip(this.title, descriptionTooltip)
     }
 
     fun setScoreChangeEvent(scoreChangeEvent: () -> Unit) {
