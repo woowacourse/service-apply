53b41a4154c6dabc3592623fab92a168c6b776cb
lalize <printf@kakao.com>
2020-10-07 16:26:44 +0900

    fix: convert to hex string

--- src/main/kotlin/support/security/Encrypts.kt
+++ src/main/kotlin/support/security/Encrypts.kt
@@ -4,4 +4,7 @@ import java.security.MessageDigest
 
 private val SHA256 = MessageDigest.getInstance("SHA-256")
 
-fun sha256Encrypt(plainText: String): String = String(SHA256.digest(plainText.toByteArray()))
+fun sha256Encrypt(plainText: String): String = bytesToHex(SHA256.digest(plainText.toByteArray()))
+
+private fun bytesToHex(bytes: ByteArray): String =
+    bytes.fold("") { previous, current -> previous + "%02x".format(current) }
