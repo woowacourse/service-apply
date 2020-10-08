74d3e8f85dace899ad7480b5c1532b02babbff39
SeungWan Park <qkrtmddhks95@gmail.com>
2020-10-08 15:09:09 +0900

    feat(selection): implement the evaluation target list view

--- src/main/kotlin/apply/application/ApplicantService.kt
+++ src/main/kotlin/apply/application/ApplicantService.kt
@@ -112,6 +112,14 @@ class ApplicantService(
                 gender = Gender.MALE,
                 birthday = createLocalDate(2020, 1, 1),
                 password = Password("password")
+            ),
+            Applicant(
+                name = "홍길동4",
+                email = "d@email.com",
+                phoneNumber = "010-0000-0000",
+                gender = Gender.MALE,
+                birthday = createLocalDate(2020, 1, 1),
+                password = Password("password")
             )
         )
         applicantRepository.saveAll(applicants)
