c59b6cfe714fea6fec4aa239c38bf6eb67a21b7d
lalize <printf@kakao.com>
2020-10-11 19:13:27 +0900

    refactor: remove dummy data

--- frontend/src/api/application-forms.js
+++ frontend/src/api/application-forms.js
@@ -3,60 +3,22 @@ import "./interceptor"
 
 const BASE_URL = "/api/application-forms"
 
-export const fetchMyApplicationForms = token => {
-  return axios
-    .get(`${BASE_URL}/me`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-    .catch(() =>
-      Promise.resolve({
-        data: [
-          {
-            id: 1,
-            referenceUrl: "https://www.google.com",
-            submitted: false,
-            recruitmentId: 1,
-          },
-          {
-            id: 2,
-            referenceUrl: "https://www.google.com",
-            submitted: false,
-            recruitmentId: 2,
-          },
-        ],
-      }),
-    )
-}
+export const fetchMyApplicationForms = token =>
+  axios.get(`${BASE_URL}/me`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+  })
 
 export const fetchForm = ({ token, recruitmentId }) =>
-  axios
-    .get(`${BASE_URL}`, {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-      params: {
-        recruitmentId,
-      },
-    })
-    .catch(() =>
-      Promise.resolve({
-        data: {
-          referenceUrl: "https://www.google.com",
-          answers: [
-            {
-              contents: "스타트업을 하고 싶습니다.",
-              recruitmentItemId: 1,
-            },
-            {
-              contents: "책임감",
-              recruitmentItemId: 2,
-            },
-          ],
-        },
-      }),
-    )
+  axios.get(`${BASE_URL}`, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    params: {
+      recruitmentId,
+    },
+  })
 
 export const createForm = ({ token, recruitmentId }) =>
   axios.post(
