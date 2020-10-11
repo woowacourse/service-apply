51f083d255407f1f74d0d4ac90ad5593e26cab30
lowoon <sangbo0229@gmail.com>
2020-10-11 17:23:31 +0900

    feat: apply review

--- src/main/kotlin/apply/log/LoggerAspect.kt
+++ src/main/kotlin/apply/log/LoggerAspect.kt
@@ -3,9 +3,10 @@ package apply.log
 import apply.utils.Log
 import com.fasterxml.jackson.databind.ObjectMapper
 import org.aspectj.lang.ProceedingJoinPoint
-import org.aspectj.lang.annotation.Around
 import org.aspectj.lang.annotation.Aspect
+import org.aspectj.lang.annotation.Before
 import org.aspectj.lang.annotation.Pointcut
+import org.aspectj.lang.reflect.MethodSignature
 import org.springframework.stereotype.Component
 import org.springframework.web.context.request.RequestContextHolder
 import org.springframework.web.context.request.ServletRequestAttributes
@@ -17,11 +18,11 @@ class LoggerAspect(val objectMapper: ObjectMapper) {
     companion object : Log
 
     @Pointcut("execution(* apply..*Controller.*(..))")
-    fun loggerPointCut() {
+    fun methodPointCut() {
     }
 
-    @Around("loggerPointCut()")
-    fun methodLogger(proceedingJoinPoint: ProceedingJoinPoint) {
+    @Before("methodPointCut()")
+    fun methodLog(proceedingJoinPoint: ProceedingJoinPoint): Any {
         val request = (RequestContextHolder.getRequestAttributes() as ServletRequestAttributes).request
 
         val controllerName = proceedingJoinPoint.signature.declaringType.simpleName
@@ -36,5 +37,19 @@ class LoggerAspect(val objectMapper: ObjectMapper) {
         params["http_method"] = request.method
 
         logger.info("{}", params)
+        return proceedingJoinPoint.proceed()
+    }
+
+    @Pointcut("execution(* apply..*Handler.*(..))")
+    fun errorPointCut() {
+    }
+
+    @Before("errorPointCut()")
+    fun errorLog(proceedingJoinPoint: ProceedingJoinPoint): Any {
+        val methodSignature = proceedingJoinPoint.signature as MethodSignature
+        val parameters = methodSignature.method.parameters
+        parameters.forEach { logger.error("message", it) }
+
+        return proceedingJoinPoint.proceed()
     }
 }
