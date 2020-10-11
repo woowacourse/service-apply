c1212a294095c8150f42ec405c50cfbe5c25fec1
lalize <printf@kakao.com>
2020-10-11 16:53:23 +0900

    refactor: set axios baseURL

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,9 @@ import Vue from "vue"
 import App from "./App.vue"
 import router from "./router"
 import store from "./store"
+import axios from "axios"
+
+axios.defaults.baseURL = process.env.VUE_APP_BACKEND_URL
 
 Vue.config.productionTip = false
 
