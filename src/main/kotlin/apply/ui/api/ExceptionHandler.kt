593d3066668864556a8692189fc6cdf4f6c19262
lowoon <sangbo0229@gmail.com>
2020-10-12 15:18:17 +0900

    refactor: log exception

--- src/main/kotlin/apply/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/apply/ui/api/ExceptionHandler.kt
@@ -14,18 +14,21 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
 
     @ExceptionHandler(value = [ApplicantValidateException::class])
     fun handleApplicantValidateException(exception: ApplicantValidateException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
             .body(ApiResponse.error(exception.message))
     }
 
     @ExceptionHandler(value = [(IllegalArgumentException::class), (IllegalStateException::class)])
     fun handleRuntimeException(exception: RuntimeException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
         return ResponseEntity.status(HttpStatus.BAD_REQUEST)
             .body(ApiResponse.error(exception.message))
     }
 
     @ExceptionHandler(value = [Exception::class])
     fun handleGlobalException(exception: Exception): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ApiResponse.error(exception.message))
     }
