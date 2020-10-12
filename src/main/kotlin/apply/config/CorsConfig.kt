927f8d144f60dd0b41b990b148d807fbd3dce126
woowahan-pjs <jason@woowahan.com>
2020-10-13 02:01:02 +0900

    polish code

--- src/main/kotlin/apply/config/CorsConfig.kt
+++ src/main/kotlin/apply/config/CorsConfig.kt
@@ -1,6 +1,7 @@
 package apply.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
@@ -8,6 +9,17 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/api/**")
-            .allowedMethods("GET", "HEAD", "POST", "PUT", "PATCH", "DELETE")
+            .allowedMethods(*PERMIT_METHODS.map(HttpMethod::name).toTypedArray())
+    }
+
+    companion object {
+        private val PERMIT_METHODS: List<HttpMethod> = listOf(
+            HttpMethod.GET,
+            HttpMethod.HEAD,
+            HttpMethod.POST,
+            HttpMethod.PUT,
+            HttpMethod.PATCH,
+            HttpMethod.DELETE
+        )
     }
 }
