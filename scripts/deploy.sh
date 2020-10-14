6ca17c32c7c48d04f51fcb06ba6acfb37c128d49
woowahan-pjs <jason@woowahan.com>
2020-10-15 01:57:27 +0900

    fix(build): change the order of terminate and copy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,6 @@
 BUILD_PATH=$(ls /home/ubuntu/build/*.jar)
 JAR_NAME=$(basename $BUILD_PATH)
 
-DEPLOY_PATH=/home/ubuntu/
-cp $BUILD_PATH $DEPLOY_PATH
-
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
@@ -14,6 +11,9 @@ else
   kill -15 $CURRENT_PID
   sleep 5
 fi
+
+DEPLOY_PATH=/home/ubuntu/
+cp $BUILD_PATH $DEPLOY_PATH
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
 nohup java -jar $DEPLOY_JAR > /dev/null 2> /dev/null < /dev/null &
