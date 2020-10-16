e14c4ad1122da8fd346e32d87852c016fc06e27a
woowahan-pjs <jason@woowahan.com>
2020-10-16 13:49:12 +0900

    fix(properties): make it immutable

--- src/main/kotlin/apply/Application.kt
+++ src/main/kotlin/apply/Application.kt
@@ -1,10 +1,12 @@
 package apply
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableAsync
 
 @EnableAsync
+@ConfigurationPropertiesScan
 @SpringBootApplication
 class Application
 
