09bb797fa3007b92efc476580f41dc866dd784b2
woowahan-pjs <jason@woowahan.com>
2020-10-20 20:23:08 +0900

    feat(docker): add local db container

--- src/main/kotlin/apply/domain/recruitmentitem/Answers.kt
+++ src/main/kotlin/apply/domain/recruitmentitem/Answers.kt
@@ -1,5 +1,6 @@
 package apply.domain.recruitmentitem
 
+import javax.persistence.CollectionTable
 import javax.persistence.ElementCollection
 import javax.persistence.Embeddable
 import javax.persistence.FetchType
@@ -7,5 +8,6 @@ import javax.persistence.FetchType
 @Embeddable
 class Answers(
     @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "application_form_answers")
     val items: MutableList<Answer> = mutableListOf()
 )
