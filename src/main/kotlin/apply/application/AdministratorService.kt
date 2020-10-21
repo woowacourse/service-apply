8ca738090064718b5b0b1deb8f97f790d53825f1
Andy H. Jung <commaniakr@gmail.com>
2020-10-21 11:41:40 +0900

    feat: populated dummy administrator account

--- src/main/kotlin/apply/application/AdministratorService.kt
+++ src/main/kotlin/apply/application/AdministratorService.kt
@@ -1,15 +1,30 @@
 package apply.application
 
+import apply.domain.administrator.Administrator
 import apply.domain.administrator.AdministratorRepository
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
+import javax.annotation.PostConstruct
 
 @Service
 class AdministratorService(private val administratorRepository: AdministratorRepository) : UserDetailsService {
     @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(name: String): UserDetails {
         return administratorRepository.findByName(name)
+    }
+
+    @PostConstruct
+    private fun populateDummy() {
+        if (administratorRepository.count() != 0L) {
+            return
+        }
+        val administrator = Administrator(
+            id = 1L,
+            name = "admin",
+            password = "{noop}1234"
+        )
+        administratorRepository.save(administrator)
     }
 }
