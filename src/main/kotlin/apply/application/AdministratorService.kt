9028ddffef0ad59b24c8616aa8c60a366f1aa617
Andy H. Jung <commaniakr@gmail.com>
2020-10-21 19:51:53 +0900

    refactor: applied elvis operator for UsernameNotFoundException

--- src/main/kotlin/apply/application/AdministratorService.kt
+++ src/main/kotlin/apply/application/AdministratorService.kt
@@ -10,9 +10,8 @@ import javax.annotation.PostConstruct
 
 @Service
 class AdministratorService(private val administratorRepository: AdministratorRepository) : UserDetailsService {
-    @Throws(UsernameNotFoundException::class)
     override fun loadUserByUsername(name: String): UserDetails {
-        return administratorRepository.findByName(name)
+        return administratorRepository.findByName(name) ?: throw UsernameNotFoundException("사용자를 찾을 수 없습니다.")
     }
 
     @PostConstruct
--- src/main/kotlin/apply/domain/administrator/AdministratorRepository.kt
+++ src/main/kotlin/apply/domain/administrator/AdministratorRepository.kt
@@ -3,5 +3,5 @@ package apply.domain.administrator
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AdministratorRepository : JpaRepository<Administrator, Long> {
-    fun findByName(name: String): Administrator
+    fun findByName(name: String): Administrator?
 }
