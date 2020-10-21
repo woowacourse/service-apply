05e57367f90865b6278b5d674c5ea00a9ec3a24d
Andy H. Jung <commaniakr@gmail.com>
2020-10-21 11:17:21 +0900

    refactor: changed ConfigureUIServiceInitListener to Kotlin

---
+++ src/main/kotlin/apply/security/ConfigureUIServiceInitListener.kt
@@ -0,0 +1,25 @@
+package apply.security
+
+import apply.security.SecurityUtils.isUserLoggedIn
+import apply.ui.admin.LoginView
+import com.vaadin.flow.router.BeforeEnterEvent
+import com.vaadin.flow.server.ServiceInitEvent
+import com.vaadin.flow.server.UIInitEvent
+import com.vaadin.flow.server.VaadinServiceInitListener
+import org.springframework.stereotype.Component
+
+@Component
+class ConfigureUIServiceInitListener : VaadinServiceInitListener {
+    override fun serviceInit(event: ServiceInitEvent) {
+        event.source.addUIInitListener { uiEvent: UIInitEvent ->
+            val ui = uiEvent.ui
+            ui.addBeforeEnterListener { event: BeforeEnterEvent -> authenticateNavigation(event) }
+        }
+    }
+
+    private fun authenticateNavigation(event: BeforeEnterEvent) {
+        if (LoginView::class.java != event.navigationTarget && !isUserLoggedIn) {
+            event.rerouteTo(LoginView::class.java)
+        }
+    }
+}
