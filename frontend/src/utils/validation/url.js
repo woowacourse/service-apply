9146c46596678a4ea064aecc1aa0cdbe1f48736c
안형주 <a301dks@naver.com>
2020-10-22 17:27:26 +0900

    feat(apply): modify the url validation to include http or https

--- frontend/src/utils/validation/url.js
+++ frontend/src/utils/validation/url.js
@@ -1,5 +1,5 @@
-const REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
+const REGEX = /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)[a-z0-9]+([-.][a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/
 
 export const isValid = v => v === "" || REGEX.test(v)
 
-export const MESSAGE = "정확한 URL을 입력해 주세요."
+export const MESSAGE = "http(s)를 포함한 정확한 URL을 입력해 주세요."
