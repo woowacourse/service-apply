17453dfb8dfbbbb5bcb1f1e0ed7594ea9935334c
woowahan-pjs <jason@woowahan.com>
2020-10-23 10:27:02 +0900

    fix(login): roll back title to h1

--- src/main/kotlin/apply/ui/admin/LoginView.kt
+++ src/main/kotlin/apply/ui/admin/LoginView.kt
@@ -1,12 +1,12 @@
 package apply.ui.admin
 
+import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.login.LoginForm
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.router.BeforeEnterEvent
 import com.vaadin.flow.router.BeforeEnterObserver
 import com.vaadin.flow.router.Route
-import support.views.Title
 
 @Route(value = "admin/login")
 class LoginView : VerticalLayout(), BeforeEnterObserver {
@@ -17,7 +17,7 @@ class LoginView : VerticalLayout(), BeforeEnterObserver {
         alignItems = FlexComponent.Alignment.CENTER
         justifyContentMode = FlexComponent.JustifyContentMode.CENTER
         login.action = "admin/login"
-        add(Title("우아한테크코스"), login)
+        add(H1("우아한테크코스"), login)
     }
 
     override fun beforeEnter(beforeEnterEvent: BeforeEnterEvent) {
