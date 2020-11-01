64cc87245c5d89caece3bb7cc5b0285af01333d4
BEOMJUN KIM <skyblue300a@gmail.com>
2020-11-01 16:25:11 +0900

    refactor(admin): remove unnecessary dummy data

--- src/main/kotlin/apply/application/RecruitmentService.kt
+++ src/main/kotlin/apply/application/RecruitmentService.kt
@@ -73,32 +73,18 @@ class RecruitmentService(
         }
         val recruitments = listOf(
             Recruitment(
-                title = "지원할 제목",
-                startDateTime = createLocalDateTime(2020, 10, 5, 10),
-                endDateTime = createLocalDateTime(2020, 11, 5, 10),
-                recruitable = true,
-                hidden = false
-            ),
-            Recruitment(
-                title = "웹 백엔드 2기",
+                title = "웹 백엔드 3기",
                 startDateTime = createLocalDateTime(2019, 10, 25, 10),
                 endDateTime = createLocalDateTime(2019, 11, 5, 10),
                 recruitable = true,
                 hidden = false
             ),
             Recruitment(
-                title = "웹 백엔드 3기",
+                title = "웹 프론트엔드 3기",
                 startDateTime = createLocalDateTime(2020, 10, 25, 15),
                 endDateTime = createLocalDateTime(2020, 11, 5, 10),
                 recruitable = true,
                 hidden = true
-            ),
-            Recruitment(
-                title = "웹 프론트엔드 3기",
-                startDateTime = createLocalDateTime(2020, 10, 25, 15),
-                endDateTime = createLocalDateTime(2020, 11, 5, 10),
-                recruitable = false,
-                hidden = false
             )
         )
         recruitmentRepository.saveAll(recruitments)
