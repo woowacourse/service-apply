d96070f9bbb29a21cb586887dea6d6b30047117f
naeunkim <knae11@naver.com>
2021-07-03 22:24:53 +0900

    refactor(CheaterServiceTest): Mockito 테스트 Mockk 전환

--- src/test/kotlin/apply/application/CheaterServiceTest.kt
+++ src/test/kotlin/apply/application/CheaterServiceTest.kt
@@ -3,25 +3,21 @@ package apply.application
 import apply.domain.applicant.ApplicantRepository
 import apply.domain.cheater.Cheater
 import apply.domain.cheater.CheaterRepository
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
 import org.junit.jupiter.api.assertThrows
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.BDDMockito.any
-import org.mockito.BDDMockito.anyLong
-import org.mockito.BDDMockito.given
-import org.mockito.Mock
-import org.mockito.junit.jupiter.MockitoExtension
+import support.test.UnitTest
 
-@ExtendWith(MockitoExtension::class)
+@UnitTest
 internal class CheaterServiceTest {
-    @Mock
+    @MockK
     private lateinit var applicantRepository: ApplicantRepository
 
-    @Mock
+    @MockK
     private lateinit var cheaterRepository: CheaterRepository
-
     private lateinit var cheaterService: CheaterService
 
     @BeforeEach
@@ -31,14 +27,14 @@ internal class CheaterServiceTest {
 
     @Test
     fun `부정 행위자를 추가한다`() {
-        given(cheaterRepository.existsByApplicantId(anyLong())).willReturn(false)
-        given(cheaterRepository.save(any(Cheater::class.java))).willReturn(Cheater(1L))
+        every { cheaterRepository.existsByApplicantId(ofType(Long::class)) } answers { false }
+        every { cheaterRepository.save(ofType(Cheater::class)) } answers { Cheater(1L) }
         assertDoesNotThrow { cheaterService.save(1L) }
     }
 
     @Test
     fun `이미 등록된 부정 행위자를 추가하는 경우 예외를 던진다`() {
-        given(cheaterRepository.existsByApplicantId(anyLong())).willReturn(true)
+        every { cheaterRepository.existsByApplicantId(ofType(Long::class)) } answers { true }
         assertThrows<IllegalArgumentException> { cheaterService.save(1L) }
     }
 }
