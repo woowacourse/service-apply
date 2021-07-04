94f6508ff794cc168aec7a96aec1a6da98efa450
NewWisdom <wlgp2500@gmail.com>
2021-07-04 22:24:34 +0900

    feat: recruitment-controller 추가

--- src/main/kotlin/apply/ui/api/RecruitmentRestController.kt
+++ src/main/kotlin/apply/ui/api/RecruitmentRestController.kt
@@ -1,12 +1,17 @@
 package apply.ui.api
 
+import apply.application.RecruitmentData
 import apply.application.RecruitmentItemService
 import apply.application.RecruitmentResponse
 import apply.application.RecruitmentService
+import apply.domain.recruitment.Recruitment
 import apply.domain.recruitmentitem.RecruitmentItem
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -25,5 +30,32 @@ class RecruitmentRestController(
     fun findItemsById(@PathVariable("id") recruitmentId: Long): ResponseEntity<ApiResponse<List<RecruitmentItem>>> {
         return ResponseEntity.ok()
             .body(ApiResponse.success(recruitmentItemService.findByRecruitmentIdOrderByPosition(recruitmentId)))
+    }
+
+    @PostMapping
+    fun save(@RequestBody request: RecruitmentData): ResponseEntity<Unit> {
+        recruitmentService.save(request)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping
+    fun findAllRecruitments(): ResponseEntity<ApiResponse<List<Recruitment>>> {
+        return ResponseEntity.ok(ApiResponse.success(recruitmentService.findAll()))
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Long): ResponseEntity<Unit> {
+        recruitmentService.deleteById(id)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/{id}")
+    fun getById(@PathVariable id: Long): ResponseEntity<ApiResponse<Recruitment>> {
+        return ResponseEntity.ok(ApiResponse.success(recruitmentService.getById(id)))
+    }
+
+    @GetMapping("/not-ended/{id}")
+    fun getNotEndedDataById(@PathVariable id: Long): ResponseEntity<ApiResponse<RecruitmentData>> {
+        return ResponseEntity.ok(ApiResponse.success(recruitmentService.getNotEndedDataById(id)))
     }
 }
