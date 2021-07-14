42ff663329cb12816c39385e14afe4c0e9f4ef24
rok93 <goodboy302@naver.com>
2021-07-14 23:08:28 +0900

    refactor: RestDocs의 config 파일 람다식 파라미터 'it'으로 변경

--- src/test/kotlin/apply/config/RestDocsConfiguration.kt
+++ src/test/kotlin/apply/config/RestDocsConfiguration.kt
@@ -3,15 +3,14 @@ package apply.config
 import org.springframework.boot.test.autoconfigure.restdocs.RestDocsMockMvcConfigurationCustomizer
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
-import org.springframework.restdocs.mockmvc.MockMvcRestDocumentationConfigurer
 import org.springframework.restdocs.operation.preprocess.Preprocessors
 
 @TestConfiguration
 class RestDocsConfiguration {
     @Bean
     fun restDocsMockMvcConfigurationCustomizer(): RestDocsMockMvcConfigurationCustomizer {
-        return RestDocsMockMvcConfigurationCustomizer { configurer: MockMvcRestDocumentationConfigurer ->
-            configurer.operationPreprocessors()
+        return RestDocsMockMvcConfigurationCustomizer {
+            it.operationPreprocessors()
                 .withRequestDefaults(Preprocessors.prettyPrint())
                 .withResponseDefaults(Preprocessors.prettyPrint())
         }
