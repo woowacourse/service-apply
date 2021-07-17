c223d67d52dd510ccd9db04d9243eb895628c2f2
Sunny.K <sunnyk5780@gmail.com>
2021-07-17 21:30:13 +0900

    feat: Description 컴포넌트 추가

--- frontend/src/components/form/index.js
+++ frontend/src/components/form/index.js
@@ -1,3 +1,4 @@
 export { default as TextInput } from './TextInput/TextInput';
 export { default as Field } from './Field/Field';
 export { default as Label } from './Label/Label';
+export { default as Description } from './Description/Description';
