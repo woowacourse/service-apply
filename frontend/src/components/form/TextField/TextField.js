cc10934de4781ddd6ba5c20bdbf58aaf72291b4a
Sunny.K <sunnyk5780@gmail.com>
2021-07-17 22:10:20 +0900

    feat: TextField 컴포넌트 추가

---
+++ frontend/src/components/form/TextField/TextField.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Field from '../Field/Field';
+import Label from '../Label/Label';
+import Description from '../Description/Description';
+import TextInput from '../TextInput/TextInput';
+import './TextField.css';
+
+const TextField = ({
+  required,
+  label,
+  value,
+  description,
+  maxLength,
+  ...props
+}) => {
+  return (
+    <Field className="text-field">
+      <label className="text-field">
+        <div>
+          <Label required={required}>{label}</Label>
+        </div>
+        {description && <Description>{description}</Description>}
+        {maxLength > 0 && (
+          <div className="length-limit">
+            {value.length} / {maxLength}
+          </div>
+        )}
+        <TextInput
+          required={required}
+          value={value}
+          maxLength={maxLength}
+          {...props}
+        />
+      </label>
+    </Field>
+  );
+};
+
+TextField.propTypes = {
+  label: PropTypes.string,
+  value: PropTypes.string,
+  required: PropTypes.bool,
+  description: PropTypes.node,
+  maxLength: PropTypes.number,
+};
+
+export default TextField;
--- frontend/src/components/form/index.js
+++ frontend/src/components/form/index.js
@@ -1,4 +1,5 @@
-export { default as TextInput } from './TextInput/TextInput';
 export { default as Field } from './Field/Field';
 export { default as Label } from './Label/Label';
 export { default as Description } from './Description/Description';
+export { default as TextInput } from './TextInput/TextInput';
+export { default as TextField } from './TextField/TextField';
