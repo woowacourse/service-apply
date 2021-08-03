d2efbd7767a6678ef1f6027e4dc6d80cb8c2239e
woowahan-pjs <jason@woowahan.com>
2021-08-03 17:20:50 +0900

    feat(term): add dummy terms

---
+++ src/main/kotlin/apply/application/TermService.kt
@@ -0,0 +1,27 @@
+package apply.application
+
+import apply.domain.term.Term
+import apply.domain.term.TermRepository
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import javax.annotation.PostConstruct
+
+@Transactional
+@Service
+class TermService(
+    private val termRepository: TermRepository
+) {
+    @PostConstruct
+    private fun populateDummy() {
+        if (termRepository.count() != 0L) {
+            return
+        }
+        val terms = listOf(
+            Term("1기"),
+            Term("2기"),
+            Term("3기"),
+            Term("4기")
+        )
+        termRepository.saveAll(terms)
+    }
+}
