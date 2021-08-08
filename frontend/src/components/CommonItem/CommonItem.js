06cf5854b5d049a05074f068b8ecb6a4a23d1353
Sunny.K <sunnyk5780@gmail.com>
2021-08-09 05:17:41 +0900

    feat: 모집 시작 및 종료 날짜 형식 변경

--- frontend/src/components/CommonItem/CommonItem.js
+++ frontend/src/components/CommonItem/CommonItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useMemo } from "react";
 import PropTypes from "prop-types";
 
 import BaseItem from "../BaseItem/BaseItem";
@@ -6,14 +6,25 @@ import BaseItem from "../BaseItem/BaseItem";
 import { Button } from "../form";
 
 import styles from "./CommonItem.module.css";
+import { formatDateTime } from "../../utils/date";
 
 const CommonItem = ({ buttonLabel, activeButton, recruitment, goPage }) => {
+  const formattedStartDateTime = useMemo(
+    () => formatDateTime(new Date(recruitment.startDateTime)),
+    [recruitment.startDateTime]
+  );
+
+  const formattedEndDateTime = useMemo(
+    () => formatDateTime(new Date(recruitment.endDateTime)),
+    [recruitment.endDateTime]
+  );
+
   return (
     <div className={styles["common-item"]}>
       <BaseItem
         title={recruitment.title}
-        startDateTime={recruitment.startDateTime}
-        endDateTime={recruitment.endDateTime}
+        startDateTime={formattedStartDateTime}
+        endDateTime={formattedEndDateTime}
       />
       <Button className="button" disabled={!activeButton} onClick={goPage}>
         {buttonLabel}
