b80f9b46582f2d790757349c8ed4cd97fe575793
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-03 04:02:25 +0900

    feat: csv 컨텐츠를 생성하는 서비스 추가

--- src/main/kotlin/apply/application/CsvService.kt
+++ src/main/kotlin/apply/application/CsvService.kt
@@ -1,9 +1,32 @@
 package apply.application
 
+import apply.domain.evaluationItem.EvaluationItemRepository
 import apply.utils.CsvGenerator
+import apply.utils.CsvRow
+import java.io.ByteArrayInputStream
 
 class CsvService(
-    val csvGenerator: CsvGenerator
+    private val applicantService: ApplicantService,
+    private val evaluationTargetService: EvaluationTargetService,
+    private val evaluationItemRepository: EvaluationItemRepository,
+    private val csvGenerator: CsvGenerator
 ) {
+    fun createTargetCsv(evaluationId: Long): ByteArrayInputStream {
+        val targets = evaluationTargetService.findAllByEvaluationIdAndKeyword(evaluationId)
+        val titles =
+            evaluationItemRepository.findByEvaluationIdOrderByPosition(evaluationId)
+                .map { it.title + "(" + it.maximumScore + ")" }.toTypedArray()
+        val headerTitles = arrayOf("applicantId", "이름", "이메일", *titles, "평가 상태")
 
+        val csvRows = targets.map {
+            CsvRow(
+                applicantService.getByEmail(it.email).id.toString(),
+                it.name,
+                it.email,
+                *it.answers.map { item -> item.score.toString() }.toTypedArray(),
+                it.evaluationStatus.name
+            )
+        }
+        return csvGenerator.generateBy(headerTitles, csvRows)
+    }
 }
