997b69715be00e8ea2bf8724b24ea3601ad8e8bd
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-03 04:01:33 +0900

    feat: csvGenerator 기능 추가

--- src/main/kotlin/apply/utils/CsvGenerator.kt
+++ src/main/kotlin/apply/utils/CsvGenerator.kt
@@ -1,14 +1,20 @@
 package apply.utils
 
+import org.apache.commons.csv.CSVFormat
+import org.apache.commons.csv.CSVPrinter
 import org.springframework.stereotype.Component
 import java.io.ByteArrayInputStream
 
 @Component
 class CsvGenerator {
     fun generateBy(headerTitles: Array<String>, rows: List<CsvRow>): ByteArrayInputStream {
-        val data = ""
 
-        val csvContent = headerTitles.joinToString(",")
-        return ByteArrayInputStream(csvContent.toByteArray())
+        val csvContent = StringBuilder()
+
+        val csvPrinter = CSVPrinter(csvContent, CSVFormat.DEFAULT)
+        csvPrinter.printRecords(headerTitles)
+        rows.map { it -> csvPrinter.printRecord(it.data) }
+
+        return ByteArrayInputStream(csvContent.toString().toByteArray())
     }
 }
--- src/test/kotlin/apply/utils/CsvGeneratorTest.kt
+++ src/test/kotlin/apply/utils/CsvGeneratorTest.kt
@@ -10,6 +10,7 @@ class CsvGeneratorTest {
     @Test
     fun `Csv 파일의 내용을 생성한다`() {
         //given
+        val lineSeparator = System.lineSeparator()
         val headers: Array<String> = arrayOf("header1", "header2", "header3")
         val csvRow: List<CsvRow> =
             listOf(
@@ -18,10 +19,16 @@ class CsvGeneratorTest {
                 CsvRow("csvContent7", "csvContent8", "csvContent9")
             )
         val csvGenerator = CsvGenerator()
-
+        val expected = "header1,header2,header3" + lineSeparator +
+            "csvContent1,csvContent2,csvContent3" + lineSeparator +
+            "csvContent4,csvContent5,csvContent6" + lineSeparator +
+            "csvContent7,csvContent8,csvContent9"
+        //when
+        val generatedCsvContent: ByteArrayInputStream = csvGenerator.generateBy(headers, csvRow)
         //then
-        val generateBy: ByteArrayInputStream = csvGenerator.generateBy(headers, csvRow)
-        val bufferedReader = BufferedReader(generateBy.reader())
-        assertThat(bufferedReader.readLine()).isEqualTo("header1,header2,header3")
+        val bufferedReader = BufferedReader(generatedCsvContent.reader())
+
+        assertThat(expected)
+            .isEqualTo(bufferedReader.readLines().joinToString(lineSeparator))
     }
 }
