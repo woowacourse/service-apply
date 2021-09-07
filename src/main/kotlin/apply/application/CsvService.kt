3414d840251fff6ced1624806285e5a5c2166b34
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-07 19:13:44 +0900

    refactor: csv 파일에 평가되지 않은 항목은 0으로 표시하도록 변경

--- src/main/kotlin/apply/application/CsvService.kt
+++ src/main/kotlin/apply/application/CsvService.kt
@@ -1,5 +1,6 @@
 package apply.application
 
+import apply.domain.evaluationItem.EvaluationItem
 import apply.domain.evaluationItem.EvaluationItemRepository
 import apply.utils.CsvGenerator
 import apply.utils.CsvRow
@@ -16,19 +17,26 @@ class CsvService(
     private val csvGenerator: CsvGenerator
 ) {
     fun createTargetCsv(evaluationId: Long): ByteArrayInputStream {
-        val targets = evaluationTargetService.findAllByEvaluationIdAndKeyword(evaluationId)
-        val titles =
+        val targets: List<EvaluationTargetResponse> =
+            evaluationTargetService.findAllByEvaluationIdAndKeyword(evaluationId)
+        val evaluationItems: List<EvaluationItem> =
             evaluationItemRepository.findByEvaluationIdOrderByPosition(evaluationId)
-                .map { it.title + "(" + it.maximumScore + ")" }.toTypedArray()
-        val headerTitles = arrayOf("applicantId", "이름", "이메일", "평가 상태", *titles)
+        val titles: Array<String> = evaluationItems.map { it.title + "(" + it.maximumScore + ")" }.toTypedArray()
+        val scores = LinkedHashMap<Long, String>()
 
+        val headerTitles = arrayOf("applicantId", "이름", "이메일", "평가 상태", *titles)
         val csvRows = targets.map {
+            evaluationItems.associateTo(scores) { it.id to "0" }
+            it.answers.map { item ->
+                scores.put(item.evaluationItemId, item.score.toString())
+            }
+            val values = scores.values.toTypedArray()
             CsvRow(
                 applicantService.getByEmail(it.email).id.toString(),
                 it.name,
                 it.email,
                 it.evaluationStatus.name,
-                *it.answers.map { item -> item.score.toString() }.toTypedArray()
+                *values
             )
         }
         return csvGenerator.generateBy(headerTitles, csvRows)
