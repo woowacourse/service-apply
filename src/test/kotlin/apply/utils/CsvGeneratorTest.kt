9e83a1b8e1a0f66bde81161b308a72209c352681
woowahan-pjs <jason@woowahan.com>
2021-09-08 09:31:50 +0900

    polish the code

--- src/test/kotlin/apply/utils/CsvGeneratorTest.kt
+++ src/test/kotlin/apply/utils/CsvGeneratorTest.kt
@@ -6,10 +6,9 @@ import java.io.BufferedReader
 import java.io.ByteArrayInputStream
 
 class CsvGeneratorTest {
-
     @Test
     fun `Csv 파일의 내용을 생성한다`() {
-        //given
+        // given
         val lineSeparator = System.lineSeparator()
         val headers: Array<String> = arrayOf("header1", "header2", "header3")
         val csvRow: List<CsvRow> =
@@ -23,9 +22,9 @@ class CsvGeneratorTest {
             "csvContent1,csvContent2,csvContent3" + lineSeparator +
             "csvContent4,csvContent5,csvContent6" + lineSeparator +
             "csvContent7,csvContent8,csvContent9"
-        //when
+        // when
         val generatedCsvContent: ByteArrayInputStream = csvGenerator.generateBy(headers, csvRow)
-        //then
+        // then
         val bufferedReader = BufferedReader(generatedCsvContent.reader())
 
         assertThat(expected)
