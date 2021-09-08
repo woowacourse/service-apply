7fc285354590d9fd1f00dbc2d235e458329783ea
MyaGya <38939015+MyaGya@users.noreply.github.com>
2021-09-08 13:48:13 +0900

    refactor: 테스트 코드 리펙토링

--- src/test/kotlin/apply/utils/CsvGeneratorTest.kt
+++ src/test/kotlin/apply/utils/CsvGeneratorTest.kt
@@ -3,31 +3,28 @@ package apply.utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.io.BufferedReader
-import java.io.ByteArrayInputStream
 
 class CsvGeneratorTest {
     @Test
     fun `Csv 파일의 내용을 생성한다`() {
         // given
-        val lineSeparator = System.lineSeparator()
-        val headers: Array<String> = arrayOf("header1", "header2", "header3")
-        val csvRow: List<CsvRow> =
-            listOf(
-                CsvRow("csvContent1", "csvContent2", "csvContent3"),
-                CsvRow("csvContent4", "csvContent5", "csvContent6"),
-                CsvRow("csvContent7", "csvContent8", "csvContent9")
-            )
-        val csvGenerator = CsvGenerator()
-        val expected = "header1,header2,header3" + lineSeparator +
-            "csvContent1,csvContent2,csvContent3" + lineSeparator +
-            "csvContent4,csvContent5,csvContent6" + lineSeparator +
-            "csvContent7,csvContent8,csvContent9"
-        // when
-        val generatedCsvContent: ByteArrayInputStream = csvGenerator.generateBy(headers, csvRow)
-        // then
-        val bufferedReader = BufferedReader(generatedCsvContent.reader())
+        val headers = arrayOf("header1", "header2", "header3")
+        val csvRows = listOf(
+            CsvRow("csvContent1", "csvContent2", "csvContent3"),
+            CsvRow("csvContent4", "csvContent5", "csvContent6"),
+            CsvRow("csvContent7", "csvContent8", "csvContent9")
+        )
 
-        assertThat(expected)
-            .isEqualTo(bufferedReader.readLines().joinToString(lineSeparator))
+        // when
+        val actual = CsvGenerator().generateBy(headers, csvRows)
+
+        // then
+        val expected = listOf(
+            "header1,header2,header3",
+            "csvContent1,csvContent2,csvContent3",
+            "csvContent4,csvContent5,csvContent6",
+            "csvContent7,csvContent8,csvContent9"
+        )
+        assertThat(BufferedReader(actual.reader()).readLines()).isEqualTo(expected)
     }
 }
