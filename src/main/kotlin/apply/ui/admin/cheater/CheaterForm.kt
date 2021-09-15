eba6d4dcf18d7192dc320e18fdbe9d0344ef0838
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2021-09-15 18:16:59 +0900

    feat(cheater): implement cheater registration form view

---
+++ src/main/kotlin/apply/ui/admin/cheater/CheaterForm.kt
@@ -0,0 +1,46 @@
+package apply.ui.admin.cheater
+
+import apply.application.ApplicantResponse
+import apply.application.CheaterData
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.select.Select
+import com.vaadin.flow.component.textfield.TextArea
+import support.views.BindingFormLayout
+import support.views.createItemSelect
+import support.views.createSearchBar
+
+class CheaterForm() : BindingFormLayout<CheaterData>(CheaterData::class) {
+    private val applicants: Select<ApplicantResponse> = createItemSelect<ApplicantResponse>().apply {
+        setTextRenderer { "${it.name}/${it.email}" }
+    }
+    private val description: TextArea = TextArea("등록 사유").apply {
+        placeholder = "사유를 입력하세요."
+    }
+
+    constructor(listener: (String) -> List<ApplicantResponse>) : this() {
+        add(createApplicantSearchBar(listener), description)
+        setResponsiveSteps(ResponsiveStep("0", 1))
+        drawRequired()
+    }
+
+    private fun createApplicantSearchBar(listener: (String) -> List<ApplicantResponse>): Component {
+        val searchBar = createSearchBar("회원 검색") {
+            applicants.setItems(listener(it))
+        }
+        return HorizontalLayout(searchBar, applicants).apply {
+            defaultVerticalComponentAlignment = FlexComponent.Alignment.END
+        }
+    }
+
+    override fun bindOrNull(): CheaterData? {
+        return bindDefaultOrNull()?.apply {
+            email = applicants.value.email
+        }
+    }
+
+    override fun fill(data: CheaterData) {
+        fillDefault(data)
+    }
+}
---
+++ src/main/kotlin/apply/ui/admin/cheater/CheaterFormDialog.kt
@@ -0,0 +1,68 @@
+package apply.ui.admin.cheater
+
+import apply.application.ApplicantService
+import apply.application.CheaterService
+import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.dialog.Dialog
+import com.vaadin.flow.component.html.H2
+import com.vaadin.flow.component.orderedlayout.FlexComponent
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout
+import com.vaadin.flow.component.orderedlayout.VerticalLayout
+import support.views.createContrastButton
+import support.views.createNotification
+import support.views.createPrimaryButton
+
+class CheaterFormDialog(
+    private val applicantService: ApplicantService,
+    private val cheaterService: CheaterService,
+    reloadComponents: () -> Unit
+) : Dialog() {
+    private val title: H2 = H2("부정 행위자 등록")
+    private val cheaterRegistrationForm: CheaterForm = CheaterForm { applicantService.findAllByKeyword(it) }
+
+    init {
+        add(createHeader(), cheaterRegistrationForm, createButtons(reloadComponents))
+        width = "800px"
+        height = "60%"
+        open()
+    }
+
+    private fun createHeader(): VerticalLayout {
+        return VerticalLayout(title).apply {
+            alignItems = FlexComponent.Alignment.CENTER
+            isPadding = false
+            element.style.set("margin-bottom", "50px")
+        }
+    }
+
+    private fun createButtons(reloadComponent: () -> Unit): Component {
+        return HorizontalLayout(getCreateAddButton(reloadComponent), createCancelButton()).apply {
+            setSizeFull()
+            justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+            element.style.set("margin-top", "20px")
+        }
+    }
+
+    private fun getCreateAddButton(reloadComponent: () -> Unit): Button {
+        return createPrimaryButton("저장") {
+            try {
+                cheaterRegistrationForm.bindOrNull()?.let {
+                    cheaterService.save(it)
+                    reloadComponent()
+                    close()
+                }
+            } catch (e: IllegalArgumentException) {
+                createNotification("이미 등록된 부정 행위자입니다.").open()
+            } catch (e: NullPointerException) {
+                createNotification("대상을 선택해야 합니다.").open()
+            }
+        }
+    }
+
+    private fun createCancelButton(): Button {
+        return createContrastButton("취소") {
+            close()
+        }
+    }
+}
