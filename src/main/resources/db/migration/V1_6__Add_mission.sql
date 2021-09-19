c73c3ddb66feed61a6ef2dd432cb237d699ae238
NewWisdom <wlgp2500@gmail.com>
2021-09-20 00:21:49 +0900

    feat(db): flyway 스크립트 추가

---
+++ src/main/resources/db/migration/V1_6__Add_mission.sql
@@ -0,0 +1,13 @@
+create table mission
+(
+    id   bigint       not null auto_increment,
+    title varchar(255) not null,
+    description varchar(255) not null,
+    recruitment_id bigint not null,
+    evaluation_id bigint not null,
+    submittable bit not null,
+    end_date_time datetime(6) not null,
+    start_date_time datetime(6) not null,
+    primary key (id)
+) engine = InnoDB
+  default charset = utf8mb4;
