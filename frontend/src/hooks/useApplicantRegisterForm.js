1ba7bc6c1b12183924f26eea6a663ea63ba4eaab
jho2301 <jho2301@gmail.com>
2021-09-23 10:24:50 +0900

    refactor(apply): separate setting hyphen function from handler function

--- frontend/src/hooks/useApplicantRegisterForm.js
+++ frontend/src/hooks/useApplicantRegisterForm.js
@@ -2,6 +2,16 @@ import { useState } from "react";
 
 const MAX_PHONE_NUMBER_LENGTH = 13;
 
+const setHyphen = (str, firstHyphenIdx, secondHyphenIdx) =>
+  str
+    .replaceAll("-", "")
+    .split("")
+    .map((el, idx) =>
+      idx === firstHyphenIdx || idx === secondHyphenIdx ? ["-", el] : el
+    )
+    .flat()
+    .join("");
+
 const useApplicantRegisterForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
 
@@ -11,18 +21,10 @@ const useApplicantRegisterForm = () => {
   }) => {
     if (Number.isNaN(data) || value.length > MAX_PHONE_NUMBER_LENGTH) return;
 
-    let firstHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 3 : 2;
-    let secondHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 7 : 6;
+    const firstHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 3 : 2;
+    const secondHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 7 : 6;
 
-    const result = value
-      .replaceAll("-", "")
-      .split("")
-      .map((el, idx) =>
-        idx === firstHyphenIdx || idx === secondHyphenIdx ? ["-", el] : el
-      )
-      .flat()
-      .join("")
-      .trim();
+    const result = setHyphen(value, firstHyphenIdx, secondHyphenIdx).trim();
 
     setPhoneNumber(result);
   };
