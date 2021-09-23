fbe3090977028e7882b75a09e9ccc12b79380e38
JinHwanKim <ecsimsw@gmail.com>
2021-09-23 12:05:42 +0900

    feat(evaluation): set cheaters to fail when evaluation targets are loaded

--- src/main/kotlin/apply/domain/evaluation/EvaluationRepository.kt
+++ src/main/kotlin/apply/domain/evaluation/EvaluationRepository.kt
@@ -1,6 +1,11 @@
 package apply.domain.evaluation
 
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.repository.findByIdOrNull
+
+fun EvaluationRepository.getById(evaluationId: Long): Evaluation {
+    return findByIdOrNull(evaluationId) ?: throw NoSuchElementException()
+}
 
 interface EvaluationRepository : JpaRepository<Evaluation, Long> {
     fun findAllByRecruitmentId(recruitmentId: Long): List<Evaluation>
