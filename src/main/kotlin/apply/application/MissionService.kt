5c0a77630663b620b9f7e1c5a7d67e92f7963229
NewWisdom <wlgp2500@gmail.com>
2021-09-24 16:34:51 +0900

    fix(mission): evaluation id로 변경

--- src/main/kotlin/apply/application/MissionService.kt
+++ src/main/kotlin/apply/application/MissionService.kt
@@ -13,7 +13,7 @@ class MissionService(
     private val evaluationRepository: EvaluationRepository
 ) {
     fun save(request: MissionData) {
-        check(evaluationRepository.existsById(request.id)) { "존재하지 않는 평가 id 입니다." }
+        check(evaluationRepository.existsById(request.evaluation.id)) { "존재하지 않는 평가 id 입니다." }
         check(!missionRepository.existsByEvaluationId(request.evaluation.id)) { "해당 평가에 이미 등록된 과제가 있습니다." }
         missionRepository.save(
             Mission(
