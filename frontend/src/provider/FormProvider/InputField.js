e26525f4321480bfcd559869a6554b1451b0029a
jho2301 <jho2301@gmail.com>
2021-09-24 22:25:25 +0900

    feat(apply): implement password input capslock alert

--- frontend/src/provider/FormProvider/InputField.js
+++ frontend/src/provider/FormProvider/InputField.js
@@ -3,9 +3,15 @@ import { TextField } from "../../components/form";
 import useFormContext from "../../hooks/useFormContext";
 import PropTypes from "prop-types";
 
-const InputField = ({ name, initialValue, ...props }) => {
-  const { value, errorMessage, handleChange, register, unRegister } =
-    useFormContext();
+const InputField = ({ name, initialValue, type, ...props }) => {
+  const {
+    value,
+    errorMessage,
+    handleChange,
+    handleKeyUp,
+    register,
+    unRegister,
+  } = useFormContext();
 
   useEffect(() => {
     register(name, initialValue);
@@ -18,9 +24,11 @@ const InputField = ({ name, initialValue, ...props }) => {
   return (
     <TextField
       name={name}
+      type={type ?? "text"}
       value={value[name]}
       errorMessage={errorMessage[name]}
       onChange={handleChange}
+      onKeyUp={type === "password" ? handleKeyUp : null}
       {...props}
     />
   );
