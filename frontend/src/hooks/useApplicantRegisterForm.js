54693e20c9a635fe0374989db34c16b793481f3d
jho2301 <jho2301@gmail.com>
2021-09-29 21:59:49 +0900

    refactor: 하이픈 인덱스 상수화

--- frontend/src/hooks/useApplicantRegisterForm.js
+++ frontend/src/hooks/useApplicantRegisterForm.js
@@ -2,6 +2,8 @@ import { useState } from "react";
 import { formatHyphen } from "../utils/format/phoneNumber";
 
 const MAX_PHONE_NUMBER_LENGTH = 13;
+const MAX_LENGTH_PHONE_NUMBER_HYPHEN_IDX = [3, 7];
+const PHONE_NUMBER_HYPHEN_IDX = [2, 6];
 
 const useApplicantRegisterForm = () => {
   const [phoneNumber, setPhoneNumber] = useState("");
@@ -12,8 +14,10 @@ const useApplicantRegisterForm = () => {
   }) => {
     if (Number.isNaN(data) || value.length > MAX_PHONE_NUMBER_LENGTH) return;
 
-    const firstHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 3 : 2;
-    const secondHyphenIdx = value.length === MAX_PHONE_NUMBER_LENGTH ? 7 : 6;
+    const [firstHyphenIdx, secondHyphenIdx] =
+      value.length !== MAX_PHONE_NUMBER_LENGTH
+        ? PHONE_NUMBER_HYPHEN_IDX
+        : MAX_LENGTH_PHONE_NUMBER_HYPHEN_IDX;
 
     const result = formatHyphen(value, firstHyphenIdx, secondHyphenIdx).trim();
 
