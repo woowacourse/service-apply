f9fe94120c5a7fd2b9bb38b47e1a0389620ce166
ecsimsw <ecsimsw@gmail.com>
2021-10-04 05:04:40 +0900

    refactor : 불필요한 @Repository 제거

--- src/main/kotlin/apply/domain/assignment/AssignmentRepository.kt
+++ src/main/kotlin/apply/domain/assignment/AssignmentRepository.kt
@@ -1,9 +1,7 @@
 package apply.domain.assignment
 
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.stereotype.Repository
 
-@Repository
 interface AssignmentRepository : JpaRepository<Assignment, Long> {
     fun existsByMissionIdAndApplicantId(missionId: Long, applicantId: Long): Boolean
 }
