c0b3d96b88d1411593ea0959145a8c96ec7122a5
SunYoungKwon <sunnyk5780@gmail.com>
2021-10-06 06:07:56 +0900

    chore: Button 컴포넌트 스토리북에 disabled 경우 추가

--- frontend/src/components/@common/Button/Button.stories.js
+++ frontend/src/components/@common/Button/Button.stories.js
@@ -13,8 +13,14 @@ Default.args = {
   children: "버튼",
 };
 
+export const Disabled = Template.bind({});
+Disabled.args = {
+  children: "버튼",
+  disabled: true,
+};
+
 export const Cancel = Template.bind({});
 Cancel.args = {
-  children: "버튼",
+  children: "취소",
   cancel: true,
 };
