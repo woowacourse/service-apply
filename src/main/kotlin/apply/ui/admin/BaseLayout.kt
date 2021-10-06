b56fb6792fd345dfab7a0a8e7000429134fe5d42
woowahan-pjs <jason@woowahan.com>
2021-10-06 17:50:09 +0900

    feat(apply): change to accordion tabs

--- src/main/kotlin/apply/ui/admin/BaseLayout.kt
+++ src/main/kotlin/apply/ui/admin/BaseLayout.kt
@@ -1,14 +1,17 @@
 package apply.ui.admin
 
+import apply.application.RecruitmentResponse
+import apply.application.RecruitmentService
 import apply.ui.admin.cheater.CheatersView
 import apply.ui.admin.evaluation.EvaluationsView
 import apply.ui.admin.mail.MailFormView
-import apply.ui.admin.mission.MissionSelectionsView
 import apply.ui.admin.recruitment.RecruitmentsView
-import apply.ui.admin.selections.SelectionsView
 import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.accordion.Accordion
 import com.vaadin.flow.component.applayout.AppLayout
 import com.vaadin.flow.component.applayout.DrawerToggle
+import com.vaadin.flow.component.details.DetailsVariant
+import com.vaadin.flow.component.html.Anchor
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.html.Image
 import com.vaadin.flow.component.orderedlayout.FlexComponent
@@ -22,12 +25,14 @@ import com.vaadin.flow.theme.Theme
 import com.vaadin.flow.theme.lumo.Lumo
 
 @Theme(value = Lumo::class)
-class BaseLayout : AppLayout() {
-    private val route: Map<String, Class<out Component>> = mapOf(
+class BaseLayout(
+    private val recruitmentService: RecruitmentService
+) : AppLayout() {
+    private val route: Map<String, Any> = mapOf(
         "모집 관리" to RecruitmentsView::class.java,
         "평가 관리" to EvaluationsView::class.java,
-        "과제 관리" to MissionSelectionsView::class.java,
-        "선발 과정" to SelectionsView::class.java,
+        "과제 관리" to "/admin/missions",
+        "선발 과정" to "/admin/selections",
         "부정 행위자" to CheatersView::class.java,
         "메일 발송" to MailFormView::class.java
     )
@@ -58,20 +63,39 @@ class BaseLayout : AppLayout() {
     }
 
     private fun createMenu(): Component {
-        return Tabs().apply {
-            orientation = Tabs.Orientation.VERTICAL
-            addThemeVariants(TabsVariant.LUMO_MINIMAL)
-            add(*createMenuItems().toTypedArray())
+        val recruitments = recruitmentService.findAll()
+        val menuItems = route.map {
+            when (val target = it.value) {
+                is String -> createAccordion(it.key, recruitments.toTabs(target))
+                is Class<*> -> createRouterLinkTab(it.key, target)
+                else -> throw IllegalArgumentException()
+            }
+        }
+        return createTabs(menuItems)
+    }
+
+    private fun List<RecruitmentResponse>.toTabs(path: String): Component {
+        return createTabs(map { Tab(Anchor("$path/${it.id}", it.title)) })
+    }
+
+    private fun createAccordion(summary: String, component: Component): Component {
+        return Accordion().apply {
+            add(summary, component).addThemeVariants(DetailsVariant.REVERSE)
+            close()
         }
     }
 
-    private fun createMenuItems(): List<Component> {
-        return route.map { (title, navigationTarget) ->
-            RouterLink(title, navigationTarget)
-        }.map(::createTab)
+    private fun createRouterLinkTab(text: String, navigationTarget: Class<*>): Component {
+        @Suppress("UNCHECKED_CAST")
+        return Tab(RouterLink(text, navigationTarget as Class<out Component>))
     }
 
-    private fun createTab(component: Component): Component {
-        return Tab(component)
+    private fun createTabs(components: List<Component>): Component {
+        return Tabs().apply {
+            orientation = Tabs.Orientation.VERTICAL
+            isAutoselect = false
+            addThemeVariants(TabsVariant.LUMO_MINIMAL)
+            add(*components.toTypedArray())
+        }
     }
 }
