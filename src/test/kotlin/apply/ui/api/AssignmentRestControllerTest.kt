0da3ccca6f9e0f53be7f51f998f6c21500dffadd
ecsimsw <ecsimsw@gmail.com>
2021-10-08 03:06:53 +0900

    refactor : 과제 생성 controller test 수정

--- src/test/kotlin/apply/ui/api/AssignmentRestControllerTest.kt
+++ src/test/kotlin/apply/ui/api/AssignmentRestControllerTest.kt
@@ -1,15 +1,17 @@
 package apply.ui.api
 
 import apply.application.AssignmentService
+import apply.application.UserService
 import apply.createAssignmentRequest
 import apply.createUser
-import apply.security.LoginUserResolver
+import apply.security.JwtTokenProvider
 import com.ninjasquad.springmockk.MockkBean
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
+import org.springframework.http.HttpHeaders
 import org.springframework.http.MediaType
 import org.springframework.test.web.servlet.post
 import support.test.TestEnvironment
@@ -24,27 +26,27 @@ internal class AssignmentRestControllerTest : RestControllerTest() {
     private lateinit var assignmentService: AssignmentService
 
     @MockkBean
-    private lateinit var loginUserResolver: LoginUserResolver
+    private lateinit var jwtTokenProvider: JwtTokenProvider
+
+    @MockkBean
+    private lateinit var userService: UserService
 
     private val recruitmentId = 1L
-    private val evaluationId = 1L
     private val missionId = 1L
 
     @Test
     fun `과제물을 제출한다`() {
-        val loginApplicant = createUser()
-        every { loginUserResolver.supportsParameter(any()) } returns true
-        every { loginUserResolver.resolveArgument(any(), any(), any(), any()) } returns loginApplicant
-        every { assignmentService.create(missionId, loginApplicant.id, createAssignmentRequest()) } just Runs
+        val loginUser = createUser()
 
-        mockMvc.post(
-            "/api/recruitments/{recruitmentId}/missions/{missionId}/assignments",
-            recruitmentId,
-            evaluationId,
-            missionId
-        ) {
-            content = objectMapper.writeValueAsString(createAssignmentRequest())
+        every { jwtTokenProvider.isValidToken("valid_token") } returns true
+        every { jwtTokenProvider.getSubject("valid_token") } returns loginUser.email
+        every { userService.getByEmail(loginUser.email) } returns loginUser
+        every { assignmentService.create(missionId, loginUser.id, createAssignmentRequest()) } just Runs
+
+        mockMvc.post("/api/recruitments/{recruitmentId}/missions/{missionId}/assignments", recruitmentId, missionId) {
             contentType = MediaType.APPLICATION_JSON
+            content = objectMapper.writeValueAsString(createAssignmentRequest())
+            header(HttpHeaders.AUTHORIZATION, "Bearer valid_token")
         }.andExpect {
             status { isOk }
         }
