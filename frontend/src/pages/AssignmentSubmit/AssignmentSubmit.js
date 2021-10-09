17d23f6f0a82b30d3cf8e778f582aceafd4d9cd0
jho2301 <jho2301@gmail.com>
2021-10-09 22:40:20 +0900

    feat: 과제 제출페이지 마크업 작성

--- frontend/src/pages/AssignmentSubmit/AssignmentSubmit.js
+++ frontend/src/pages/AssignmentSubmit/AssignmentSubmit.js
@@ -1,10 +1,12 @@
 import Container from "../../components/@common/Container/Container";
+import CancelButton from "../../components/form/CancelButton/CancelButton";
 import Form from "../../components/form/Form/Form";
 import FormInput from "../../components/form/FormInput/FormInput";
 import FormTextarea from "../../components/form/FormTextarea/FormTextarea";
-import SubmitButton from "../../components/form/SubmitButton";
+import SubmitButton from "../../components/form/SubmitButton/SubmitButton";
 import useForm from "../../hooks/useForm";
 import FormProvider from "../../provider/FormProvider";
+import styles from "./AssignmentSubmit.module.css";
 
 const AssignmentSubmit = () => {
   const submit = () => {};
@@ -17,12 +19,17 @@ const AssignmentSubmit = () => {
   return (
     <Container title="프리코스 1차 과제">
       <FormProvider {...methods}>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} className={styles.form}>
           <FormInput label="GitHub ID" required />
           <FormInput label="Pull Request 주소" required />
           <FormTextarea label="과제 진행 소감" required />
-          {/* <CancelButton /> */}
-          <SubmitButton />
+          <p className={styles["info-message"]}>
+            작성하신 내용은 과제 체출 마감전까지 수정하실 수 있습니다.
+          </p>
+          <div className={styles.buttons}>
+            <CancelButton />
+            <SubmitButton />
+          </div>
         </Form>
       </FormProvider>
     </Container>
