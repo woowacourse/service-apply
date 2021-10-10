70e28ecc4f95342446107c335deb8fb08b8a7469
ecsimsw <ecsimsw@gmail.com>
2021-10-11 03:08:45 +0900

    refactor : index 순서에 맞도록 파라미터 순서 변경

--- src/main/kotlin/apply/application/AssignmentService.kt
+++ src/main/kotlin/apply/application/AssignmentService.kt
@@ -17,7 +17,7 @@ class AssignmentService(
     private val evaluationTargetRepository: EvaluationTargetRepository
 ) {
     fun create(missionId: Long, userId: Long, request: CreateAssignmentRequest) {
-        check(!assignmentRepository.existsByMissionIdAndUserId(missionId, userId)) { "이미 제출한 과제물이 존재합니다." }
+        check(!assignmentRepository.existsByUserIdAndMissionId(userId, missionId)) { "이미 제출한 과제물이 존재합니다." }
         val mission = missionRepository.getById(missionId)
         check(mission.isProgressing) { "제출 불가능한 과제입니다." }
         findEvaluationTargetOf(mission.evaluationId, userId).passIfBeforeEvaluation()
--- src/main/kotlin/apply/domain/assignment/AssignmentRepository.kt
+++ src/main/kotlin/apply/domain/assignment/AssignmentRepository.kt
@@ -3,5 +3,5 @@ package apply.domain.assignment
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface AssignmentRepository : JpaRepository<Assignment, Long> {
-    fun existsByMissionIdAndUserId(missionId: Long, userId: Long): Boolean
+    fun existsByUserIdAndMissionId(userId: Long, missionId: Long): Boolean
 }
