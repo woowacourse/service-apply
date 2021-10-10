10d8596b819276347b3fd85cf0610ef5dd1f97f7
ecsimsw <ecsimsw@gmail.com>
2021-10-10 19:24:30 +0900

    refactor : AssignmentDtos 생성 방지를 위해 interface로 수정

--- src/main/kotlin/apply/application/AssignmentDtos.kt
+++ src/main/kotlin/apply/application/AssignmentDtos.kt
@@ -3,7 +3,8 @@ package apply.application
 import javax.validation.constraints.NotBlank
 import javax.validation.constraints.Size
 
-class AssignmentDtos
+// TODO: 추가 data class 추가 시 interface 제거
+interface AssignmentDtos
 
 data class CreateAssignmentRequest(
     @field:NotBlank
