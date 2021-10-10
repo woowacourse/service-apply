613ca32b4ff44baf8f8c83774dbbc46e8cabe647
SunYoungKwon <sunnyk5780@gmail.com>
2021-10-10 17:26:24 +0900

    feat: tarnary -> ??

--- frontend/src/components/form/SubmitButton/SubmitButton.js
+++ frontend/src/components/form/SubmitButton/SubmitButton.js
@@ -4,11 +4,7 @@ import Button from "../../@common/Button/Button";
 const SubmitButton = ({ children }) => {
   const { isValid, isEmpty } = useFormContext();
 
-  return (
-    <Button disabled={!isValid || isEmpty}>
-      {children ? children : "제출"}
-    </Button>
-  );
+  return <Button disabled={!isValid || isEmpty}>{children ?? "제출"}</Button>;
 };
 
 export default SubmitButton;
