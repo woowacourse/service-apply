10aabf9342c5f6d73681227c3e8f06956ac5c9e7
naeun <knae11@naver.com>
2021-10-12 08:15:40 +0900

    refactor: flyway 정렬

--- src/main/resources/db/migration/V1_10__Add_email_history.sql
+++ src/main/resources/db/migration/V1_10__Add_email_history.sql
@@ -1,11 +1,11 @@
 create table mail_history
 (
-    id         bigint not null auto_increment,
+    id         bigint       not null auto_increment,
     subject    varchar(255) not null,
-    body       longtext not null,
+    body       longtext     not null,
     sender     varchar(255) not null,
-    recipients longtext not null,
-    sent_time  datetime(0) not null,
+    recipients longtext     not null,
+    sent_time  datetime(0)  not null,
     primary key (id)
 ) engine = InnoDB
-   default charset = utf8mb4;
+  default charset = utf8mb4;
