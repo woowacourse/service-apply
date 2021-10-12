8bd2a101e432d0fa9d18938158bea1b1c819efb2
NewWisdom <43840561+NewWisdom@users.noreply.github.com>
2021-10-12 15:45:14 +0900

    feat(user): implement user information retrieve function

--- src/main/kotlin/apply/ui/api/UserRestController.kt
+++ src/main/kotlin/apply/ui/api/UserRestController.kt
@@ -60,18 +60,18 @@ class UserRestController(
     fun generateAuthenticationCode(
         @RequestParam email: String
     ): ResponseEntity<Unit> {
-        val authenticateCode = userAuthenticationService
+        val authenticationCode = userAuthenticationService
             .generateAuthenticationCode(email)
-        mailService.sendAuthenticationCodeMail(email, authenticateCode)
+        mailService.sendAuthenticationCodeMail(email, authenticationCode)
         return ResponseEntity.noContent().build()
     }
 
     @PostMapping("/authenticate-email")
     fun authenticateEmail(
         @RequestParam email: String,
-        @RequestParam authenticateCode: String
+        @RequestParam authenticationCode: String
     ): ResponseEntity<Unit> {
-        userAuthenticationService.authenticateEmail(email, authenticateCode)
+        userAuthenticationService.authenticateEmail(email, authenticationCode)
         return ResponseEntity.noContent().build()
     }
 
@@ -81,6 +81,14 @@ class UserRestController(
     ): ResponseEntity<ApiResponse<List<UserResponse>>> {
         val users = userService.findAllByKeyword(keyword)
         return ResponseEntity.ok(ApiResponse.success(users))
+    }
+
+    @GetMapping("/me")
+    fun getMyInformation(
+        @LoginUser user: User
+    ): ResponseEntity<ApiResponse<UserResponse>> {
+        val user = userService.getInformation(user.id)
+        return ResponseEntity.ok(ApiResponse.success(user))
     }
 
     @PatchMapping("/information")
