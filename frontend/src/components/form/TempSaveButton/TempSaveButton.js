6539b617103886294ef85f610f7ceff928392570
jho2301 <jho2301@gmail.com>
2021-10-17 09:27:45 +0900

    fix: 유효하지 않은 데이터, 임시저장 불가 처리

--- frontend/src/components/form/TempSaveButton/TempSaveButton.js
+++ frontend/src/components/form/TempSaveButton/TempSaveButton.js
@@ -1,8 +1,11 @@
+import useFormContext from "../../../hooks/useFormContext";
 import Button from "../../@common/Button/Button";
 
 const TempSaveButton = ({ onSaveTemp }) => {
+  const { isValid } = useFormContext();
+
   return (
-    <Button type="button" onClick={onSaveTemp}>
+    <Button type="button" onClick={onSaveTemp} disabled={!isValid}>
       임시 저장
     </Button>
   );
