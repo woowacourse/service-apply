a10f5859fcadf5fbab65afb4433495a108ad562e
NewWisdom <wlgp2500@gmail.com>
2021-10-18 08:40:51 +0900

    feat(exception): handleMethodArgumentNotValid 오버라이딩
    - dto 예외 처리

--- src/main/kotlin/apply/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/apply/ui/api/ExceptionHandler.kt
@@ -2,10 +2,13 @@ package apply.ui.api
 
 import apply.domain.applicationform.DuplicateApplicationException
 import apply.domain.user.UserAuthenticationException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import javax.persistence.EntityNotFoundException
 
@@ -45,4 +48,18 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
             .body(ApiResponse.error(exception.message))
     }
+
+    override fun handleMethodArgumentNotValid(
+        exception: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("message", exception)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(exception.messages()))
+    }
+
+    private fun MethodArgumentNotValidException.messages(): String =
+        this.bindingResult.fieldErrors.joinToString(", ") { "${it.field}: ${it.defaultMessage}" }
 }
