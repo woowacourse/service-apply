7594335d1d13c7406d5435d43d94df50fd0158da
공원 <81607552+woowapark@users.noreply.github.com>
2021-10-20 00:55:13 +0900

    feat(apply): handle invalid tokens

--- src/main/kotlin/apply/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/apply/ui/api/ExceptionHandler.kt
@@ -1,7 +1,8 @@
 package apply.ui.api
 
 import apply.domain.applicationform.DuplicateApplicationException
-import apply.domain.user.UserAuthenticationException
+import apply.domain.user.UnidentifiedUserException
+import apply.security.LoginFailedException
 import com.fasterxml.jackson.databind.exc.InvalidFormatException
 import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
 import org.springframework.http.HttpHeaders
@@ -55,10 +56,17 @@ class ExceptionHandler : ResponseEntityExceptionHandler() {
             .body(ApiResponse.error(exception.message))
     }
 
-    @ExceptionHandler(UserAuthenticationException::class)
-    fun handleUnauthorizedException(exception: UserAuthenticationException): ResponseEntity<ApiResponse<Unit>> {
+    @ExceptionHandler(LoginFailedException::class)
+    fun handleUnauthorizedException(exception: LoginFailedException): ResponseEntity<ApiResponse<Unit>> {
         logger.error("message", exception)
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+            .body(ApiResponse.error(exception.message))
+    }
+
+    @ExceptionHandler(UnidentifiedUserException::class)
+    fun handleForbiddenException(exception: UnidentifiedUserException): ResponseEntity<ApiResponse<Unit>> {
+        logger.error("message", exception)
+        return ResponseEntity.status(HttpStatus.FORBIDDEN)
             .body(ApiResponse.error(exception.message))
     }
 
