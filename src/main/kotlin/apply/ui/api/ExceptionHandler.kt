e590f244749d16f5ae0700d6d8f734b8c1fa74af
NewWisdom <43840561+NewWisdom@users.noreply.github.com>
2021-10-19 22:25:30 +0900

    fix(exception): override request validation exception handler

--- src/main/kotlin/apply/ui/api/ExceptionHandler.kt
+++ src/main/kotlin/apply/ui/api/ExceptionHandler.kt
@@ -2,15 +2,52 @@ package apply.ui.api
 
 import apply.domain.applicationform.DuplicateApplicationException
 import apply.domain.user.UserAuthenticationException
+import com.fasterxml.jackson.databind.exc.InvalidFormatException
+import com.fasterxml.jackson.module.kotlin.MissingKotlinParameterException
+import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
 import javax.persistence.EntityNotFoundException
 
 @RestControllerAdvice
 class ExceptionHandler : ResponseEntityExceptionHandler() {
+    override fun handleHttpMessageNotReadable(
+        ex: HttpMessageNotReadableException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("message", ex)
+        val message = when (val exception = ex.cause) {
+            is MissingKotlinParameterException -> "${exception.parameter.name.orEmpty()}: 널이어서는 안됩니다"
+            is InvalidFormatException -> "${exception.path.last().fieldName.orEmpty()}: 올바른 형식이어야 합니다"
+            else -> exception?.message.orEmpty()
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(message))
+    }
+
+    override fun handleMethodArgumentNotValid(
+        ex: MethodArgumentNotValidException,
+        headers: HttpHeaders,
+        status: HttpStatus,
+        request: WebRequest
+    ): ResponseEntity<Any> {
+        logger.error("message", ex)
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(ApiResponse.error(ex.messages()))
+    }
+
+    private fun MethodArgumentNotValidException.messages(): String {
+        return bindingResult.fieldErrors.joinToString(", ") { "${it.field}: ${it.defaultMessage.orEmpty()}" }
+    }
+
     @ExceptionHandler(IllegalArgumentException::class, IllegalStateException::class)
     fun handleBadRequestException(exception: RuntimeException): ResponseEntity<ApiResponse<Unit>> {
         logger.error("message", exception)
