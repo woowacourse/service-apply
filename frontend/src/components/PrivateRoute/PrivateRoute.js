80145a50b672507b8f667f253553413f865c979c
jho2301 <jho2301@gmail.com>
2021-10-20 22:05:19 +0900

    feat: PrivateRoute 토큰 없을 시 alert 제거

--- frontend/src/components/PrivateRoute/PrivateRoute.js
+++ frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -1,12 +1,9 @@
 import { Redirect, Route } from "react-router-dom";
-import { ERROR_MESSAGE } from "../../constants/messages";
 import PATH from "../../constants/path";
 import useTokenContext from "../../hooks/useTokenContext";
 
 const PrivateRoute = ({ ...props }) => {
   const { token } = useTokenContext();
-
-  if (!token) alert(ERROR_MESSAGE.ACCESS.REQUIRED_LOGIN);
 
   return token ? <Route {...props} /> : <Redirect to={PATH.LOGIN} />;
 };
