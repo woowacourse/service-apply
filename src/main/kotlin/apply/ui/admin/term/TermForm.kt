5a24fc7aa94e378319baada00eca4ce0fe7f405d
Sehwan_Jang <68985748+Sehwan-Jang@users.noreply.github.com>
2022-07-07 13:12:46 +0900

    feat(term): implement term management view

---
+++ src/main/kotlin/apply/ui/admin/term/TermForm.kt
@@ -0,0 +1,23 @@
+package apply.ui.admin.term
+
+import apply.application.TermData
+import com.vaadin.flow.component.textfield.TextField
+import support.views.BindingIdentityFormLayout
+
+class TermForm : BindingIdentityFormLayout<TermData>(TermData::class) {
+    private val name: TextField = TextField("기수명")
+
+    init {
+        add(name)
+        setResponsiveSteps(ResponsiveStep("0", 1))
+        drawRequired()
+    }
+
+    override fun bindOrNull(): TermData? {
+        return bindDefaultOrNull()
+    }
+
+    override fun fill(term: TermData) {
+        fillDefault(term)
+    }
+}
