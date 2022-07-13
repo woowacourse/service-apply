7b7ad401fcf006bbd4de982ad6c8fd0d7687cf65
Hwang Jung Min <hwangstar156@gmail.com>
2022-07-13 19:43:04 +0900

    chore: index.js react18에 맞도록 migration 진행

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 
 import App from "./App";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById("app")
+  </React.StrictMode>
 );
