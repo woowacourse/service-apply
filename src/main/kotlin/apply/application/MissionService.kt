7ad2dd3cf1562061deffe22093fe4acfd15f64ce
summerlunaa <summerlunaa9@gmail.com>
2022-07-16 15:25:26 +0900

    refactor(Mission): change response of create api
    Co-authored-by: dongho108 <dongho108@naver.com>

--- src/main/kotlin/apply/application/MissionService.kt
+++ src/main/kotlin/apply/application/MissionService.kt
@@ -18,9 +18,9 @@ class MissionService(
     private val evaluationTargetRepository: EvaluationTargetRepository,
     private val assignmentRepository: AssignmentRepository
 ) {
-    fun save(request: MissionData) {
+    fun save(request: MissionData): Long {
         validate(request)
-        missionRepository.save(
+        val mission = missionRepository.save(
             request.let {
                 Mission(
                     it.title,
@@ -34,6 +34,7 @@ class MissionService(
                 )
             }
         )
+        return mission.id
     }
 
     private fun validate(request: MissionData) {
--- src/main/kotlin/apply/ui/api/MissionRestController.kt
+++ src/main/kotlin/apply/ui/api/MissionRestController.kt
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import java.net.URI
 
 @RestController
 @RequestMapping("/api/recruitments/{recruitmentId}")
@@ -26,8 +27,8 @@ class MissionRestController(
         @RequestBody missionData: MissionData,
         @LoginUser(administrator = true) user: User
     ): ResponseEntity<Unit> {
-        missionService.save(missionData)
-        return ResponseEntity.ok().build()
+        val saveId = missionService.save(missionData)
+        return ResponseEntity.created(URI.create("/api/recruitments/$recruitmentId/missions/$saveId")).build()
     }
 
     @GetMapping("/missions")
