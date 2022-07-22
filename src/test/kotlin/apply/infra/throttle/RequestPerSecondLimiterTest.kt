b2cbf0940b53582040984c10b691b3a78c3feef3
woowahan-pjs <jason@woowahan.com>
2022-07-22 15:24:28 +0900

    refactor(test): migrate request limiter tests to kotest

--- src/test/kotlin/apply/infra/throttle/RequestPerSecondLimiterTest.kt
+++ src/test/kotlin/apply/infra/throttle/RequestPerSecondLimiterTest.kt
@@ -1,66 +1,56 @@
 package apply.infra.throttle
 
-import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertAll
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
 
-internal class RequestPerSecondLimiterTest {
-
-    @Test
-    fun `초당 1회로 제한할 경우 1초 간격 요청은 성공한다`() {
-        val requestPerSecondLimiter = RequestPerSecondLimiter(1)
-        val baseTime = System.currentTimeMillis()
-
-        assertAll(
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1000)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 2000)).isTrue }
-        )
+class RequestPerSecondLimiterTest : StringSpec({
+    "초당 1회로 제한할 경우 1초 간격 요청은 성공한다" {
+        val limiter = RequestPerSecondLimiter(1)
+        val currentTimeMillis = System.currentTimeMillis()
+        assertSoftly(limiter) {
+            tryAcquire(currentTimeMillis).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 1000).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 2000).shouldBeTrue()
+        }
     }
 
-    @Test
-    fun `초당 1회로 제한할 경우 초당 2번 요청하면 실패한다`() {
-        val requestPerSecondLimiter = RequestPerSecondLimiter(1)
-        val baseTime = System.currentTimeMillis()
-
-        assertAll(
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 999)).isFalse },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1000)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1999)).isFalse }
-        )
+    "초당 1회로 제한할 경우 초당 2번 요청하면 실패한다" {
+        val limiter = RequestPerSecondLimiter(1)
+        val currentTimeMillis = System.currentTimeMillis()
+        assertSoftly(limiter) {
+            tryAcquire(currentTimeMillis).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 500).shouldBeFalse()
+            tryAcquire(currentTimeMillis + 1000).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 1500).shouldBeFalse()
+        }
     }
 
-    @Test
-    fun `초당 2회로 제한할 경우 초당 2회 요청은 성공한다`() {
-        val requestPerSecondLimiter = RequestPerSecondLimiter(2)
-        val baseTime = System.currentTimeMillis()
-
-        assertAll(
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1001)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1500)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 2001)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 4000)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 4000)).isTrue }
-        )
+    "초당 2회로 제한할 경우 초당 2회 요청은 성공한다" {
+        val limiter = RequestPerSecondLimiter(2)
+        val currentTimeMillis = System.currentTimeMillis()
+        assertSoftly(limiter) {
+            tryAcquire(currentTimeMillis).shouldBeTrue()
+            tryAcquire(currentTimeMillis).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 1000).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 1500).shouldBeTrue()
+            tryAcquire(currentTimeMillis + 2000).shouldBeTrue()
+        }
     }
 
-    @Test
-    fun `초당 2회로 제한할 경우 초당 3번 요청하면 실패한다`() {
-        val requestPerSecondLimiter = RequestPerSecondLimiter(2)
+    "초당 2회로 제한할 경우 초당 3번 요청하면 실패한다" {
+        val limiter = RequestPerSecondLimiter(2)
         val baseTime = System.currentTimeMillis()
-
-        assertAll(
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime)).isFalse },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1000)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1500)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 1600)).isFalse },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 2000)).isTrue },
-            { assertThat(requestPerSecondLimiter.tryAcquire(baseTime + 2400)).isFalse }
-        )
+        assertSoftly(limiter) {
+            tryAcquire(baseTime).shouldBeTrue()
+            tryAcquire(baseTime).shouldBeTrue()
+            tryAcquire(baseTime).shouldBeFalse()
+            tryAcquire(baseTime + 1000).shouldBeTrue()
+            tryAcquire(baseTime + 1500).shouldBeTrue()
+            tryAcquire(baseTime + 1600).shouldBeFalse()
+            tryAcquire(baseTime + 2000).shouldBeTrue()
+            tryAcquire(baseTime + 2100).shouldBeFalse()
+        }
     }
-}
+})
