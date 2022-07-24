ef020109ade1406d75c5f2d696cfb458d720453a
summerlunaa <summerlunaa9@gmail.com>
2022-07-24 21:26:14 +0900

    test(Term): add Term id in Term object
    Co-authored-by: dongho108 <dongho108@naver.com>

--- src/test/kotlin/apply/acceptance/fixture/TermFixture.kt
+++ src/test/kotlin/apply/acceptance/fixture/TermFixture.kt
@@ -1,9 +1,14 @@
 package apply.acceptance.fixture
 
+import apply.application.TermData
+import apply.ui.api.ApiResponse
 import io.restassured.RestAssured
 import io.restassured.http.ContentType
+import io.restassured.mapper.TypeRef
 
-class Term(val name: String, val id: Long)
+class Term(var name: String, var id: Long) {
+    constructor(termData: TermData) : this(termData.name, termData.id)
+}
 
 class TermBuilder {
     var name: String = "단독 모집"
@@ -11,21 +16,23 @@ class TermBuilder {
 
     fun build(): Term {
         val term = Term(name, id)
-        postTerm(term)
-        return term
+        return postTerm(term)
     }
 
-    private fun postTerm(term: Term) {
-        RestAssured.given()
+    private fun postTerm(term: Term): Term {
+        val savedTermData = RestAssured.given()
             .contentType(ContentType.JSON)
             .body(term)
             .`when`()
             .post("/api/terms")
+            .`as`(object : TypeRef<ApiResponse<TermData>>() {})
+            .body as TermData
+        return Term(savedTermData)
     }
 }
 
 fun term(builder: TermBuilder.() -> Unit): Term {
-    return TermBuilder().apply { id = 0L }.apply(builder).build()
+    return TermBuilder().apply(builder).build()
 }
 
 fun term(): Term {
