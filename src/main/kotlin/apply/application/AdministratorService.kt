3023f742832f72ee9fc4ca14e51d7c189020dec8
cjlee38 <cjlee38@github.com>
2022-08-23 16:40:23 +0900

    refactor: wrap findByUsername with getByUsername

--- src/main/kotlin/apply/application/AdministratorService.kt
+++ src/main/kotlin/apply/application/AdministratorService.kt
@@ -1,14 +1,14 @@
 package apply.application
 
 import apply.domain.administrator.AdministratorRepository
+import apply.domain.administrator.getByUsername
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.core.userdetails.UsernameNotFoundException
 import org.springframework.stereotype.Service
 
 @Service
 class AdministratorService(private val administratorRepository: AdministratorRepository) : UserDetailsService {
-    override fun loadUserByUsername(name: String): UserDetails {
-        return administratorRepository.findByName(name) ?: throw UsernameNotFoundException("사용자를 찾을 수 없습니다.")
+    override fun loadUserByUsername(username: String): UserDetails {
+        return administratorRepository.getByUsername(username)
     }
 }
