d411f5abeba1b82596efb86438151c05aeb74f1d
soyi47 <soyi.dev@gmail.com>
2022-08-27 01:00:01 +0900

    refactor: Footer 링크 아이콘 URL 상수 파일 분리
    (cherry picked from commit d0681c3069c7228e86bb43a3eb88d54bbd76fbdb)

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,18 +1,13 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
-const URL = {
-  LOGO: "https://woowacourse.github.io/",
-  GITHUB: "https://github.com/woowacourse/",
-  YOUTUBE: "https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang/featured?view_as=subscribe",
-  INSTAGRAM: "https://instagram.com/wooteco?igshid=YmMyMTA2M2Y",
-};
+import { FOOTER_LINK_URL } from "../../constants/url";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles["content-wrapper"]}>
-        <a target="_blank" href={URL.LOGO} rel="noreferrer noopener">
+        <a target="_blank" href={FOOTER_LINK_URL.LOGO} rel="noreferrer noopener">
           <img
             className={styles.logo}
             src="/assets/logo/logo_full_white.png"
@@ -20,15 +15,30 @@ const Footer = () => {
           />
         </a>
         <div className={styles.social}>
-          <a href={URL.GITHUB} target="_blank" rel="noreferrer noopener" aria-label="GitHub">
+          <a
+            href={FOOTER_LINK_URL.GITHUB}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="GitHub"
+          >
             <i className="ti-github" aria-hidden="true" />
           </a>
           <hr className={styles.divider} />
-          <a href={URL.YOUTUBE} target="_blank" rel="noreferrer noopener" aria-label="Youtube">
+          <a
+            href={FOOTER_LINK_URL.YOUTUBE}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Youtube"
+          >
             <i className="fa fa-youtube-play" aria-hidden="true" />
           </a>
           <hr className={styles.divider} />
-          <a href={URL.INSTAGRAM} target="_blank" rel="noreferrer noopener" aria-label="Instagram">
+          <a
+            href={FOOTER_LINK_URL.INSTAGRAM}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Instagram"
+          >
             <i class="fa fa-instagram" aria-hidden="true" />
           </a>
         </div>
