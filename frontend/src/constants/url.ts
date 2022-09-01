b8a4d471b7dd7115fb4e32e63a886b35e0a4d235
soyi47 <soyi.dev@gmail.com>
2022-09-01 14:29:49 +0900

    fix: constants/url.js -> constants/url.ts로 변경
    (cherry picked from commit 36219a05cc34a615b73438f5a5b8987065857572)

--- frontend/src/constants/url.ts
+++ frontend/src/constants/url.ts
@@ -3,4 +3,4 @@ export const FOOTER_LINK_URL = {
   GITHUB: "https://github.com/woowacourse/",
   YOUTUBE: "https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang/featured?view_as=subscribe",
   INSTAGRAM: "https://instagram.com/wooteco?igshid=YmMyMTA2M2Y",
-};
+} as const;
