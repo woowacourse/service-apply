fed5062a7c9c9fdf03713126654275eac2cfcdee
Soyi Jeon <soyi.dev@gmail.com>
2022-09-22 13:05:17 +0900

    feat(apply): change header and footer component styles and links

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -1,36 +1,46 @@
 import React from "react";
 import styles from "./Footer.module.css";
 
+import { FOOTER_LINK_URL } from "../../constants/url";
+
 const Footer = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles["content-wrapper"]}>
-        <a target="_blank" href="https://woowacourse.github.io/" rel="noreferrer noopener">
+        <a target="_blank" href={FOOTER_LINK_URL.LOGO} rel="noreferrer noopener">
           <img
             className={styles.logo}
-            src="/assets/logo/logo_full_white.png"
+            src="/assets/logo/logo_full_light.png"
             alt="우아한테크코스 로고"
           />
         </a>
-        <p className={styles.copyright}>Copyright 2021. 우아한테크코스. All rights reserved.</p>
         <div className={styles.social}>
           <a
-            href="https://github.com/woowacourse/"
+            href={FOOTER_LINK_URL.GITHUB}
             target="_blank"
             rel="noreferrer noopener"
             aria-label="GitHub"
           >
-            <i className="ti-github" aria-hidden="true"></i>
+            <i className="ti-github" aria-hidden="true" />
           </a>
           <a
-            href="https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang/featured?view_as=subscribe"
+            href={FOOTER_LINK_URL.YOUTUBE}
             target="_blank"
             rel="noreferrer noopener"
             aria-label="Youtube"
           >
-            <i className="fa fa-youtube-play" aria-hidden="true"></i>
+            <i className="fa fa-youtube-play" aria-hidden="true" />
+          </a>
+          <a
+            href={FOOTER_LINK_URL.INSTAGRAM}
+            target="_blank"
+            rel="noreferrer noopener"
+            aria-label="Instagram"
+          >
+            <i className="fa fa-instagram" aria-hidden="true" />
           </a>
         </div>
+        <p className={styles.copyright}>Copyright 우아한테크코스. All rights reserved.</p>
       </div>
     </footer>
   );
---
+++ frontend/src/constants/url.ts
@@ -0,0 +1,6 @@
+export const FOOTER_LINK_URL = {
+  LOGO: "https://woowacourse.github.io/",
+  GITHUB: "https://github.com/woowacourse/",
+  YOUTUBE: "https://www.youtube.com/channel/UC-mOekGSesms0agFntnQang/featured?view_as=subscribe",
+  INSTAGRAM: "https://instagram.com/wooteco?igshid=YmMyMTA2M2Y",
+} as const;
