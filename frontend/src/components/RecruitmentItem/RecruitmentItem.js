32ec9ef8e0899d096bc43293564adeca8aedfb5a
Yunho <ansghkdbsgh@gmail.com>
2022-09-26 19:28:38 +0900

    feat: RecruitmentItem에 StatusIndicator 적용

--- frontend/src/components/RecruitmentItem/RecruitmentItem.js
+++ frontend/src/components/RecruitmentItem/RecruitmentItem.js
@@ -1,19 +1,23 @@
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import React, { useMemo } from "react";
+import { useMemo } from "react";
 import CalendarIcon from "../../assets/icon/calendar-icon.svg";
+import { RECRUITMENT_STATUS } from "../../constants/recruitment";
 import { formatDateTime } from "../../utils/format/date";
-import Button from "../@common/Button/Button";
+import StatusIndicator from "../@common/StatusIndicator/StatusIndicator";
 import styles from "./RecruitmentItem.module.css";
 
-const RecruitmentItem = ({
-  recruitment,
-  buttonLabel,
-  isButtonDisabled,
-  onClickButton,
-  className,
-  ...props
-}) => {
+const INDICATOR_LABEL = {
+  [RECRUITMENT_STATUS.RECRUITING]: "모집 중",
+  [RECRUITMENT_STATUS.RECRUITABLE]: "모집 예정",
+  [RECRUITMENT_STATUS.UNRECRUITABLE]: "일시 중지",
+  [RECRUITMENT_STATUS.ENDED]: "모집 종료",
+};
+
+const RecruitmentItem = ({ recruitment, onClickButton, className, ...props }) => {
+  const active = recruitment.status === RECRUITMENT_STATUS.RECRUITING;
+  const indicatorText = INDICATOR_LABEL[recruitment.status];
+
   const formattedStartDateTime = useMemo(
     () => (recruitment.startDateTime ? formatDateTime(new Date(recruitment.startDateTime)) : ""),
     [recruitment.startDateTime]
@@ -25,24 +29,19 @@ const RecruitmentItem = ({
   );
 
   return (
-    <div className={classNames(styles["content-wrapper"], className)} {...props}>
-      <div className={styles["text-container"]}>
-        <p className={styles.title}>
-          <strong>{recruitment.title}</strong>
+    <div
+      className={classNames(styles["content-wrapper"], className, active ? styles.active : "")}
+      {...props}
+      onClick={active ? onClickButton : () => {}}
+    >
+      <h4 className={classNames(styles.title)}>{recruitment.title}</h4>
+      <div className={styles.date}>
+        <img src={CalendarIcon} alt="달력 아이콘" className={styles.icon} />
+        <p>
+          {formattedStartDateTime} ~ {formattedEndDateTime}
         </p>
-        <div className={styles.date}>
-          <img src={CalendarIcon} alt="달력 아이콘" className={styles.icon} />
-          <p>
-            {formattedStartDateTime} ~ {formattedEndDateTime}
-          </p>
-        </div>
       </div>
-
-      {buttonLabel && (
-        <Button className={styles.button} disabled={isButtonDisabled} onClick={onClickButton}>
-          {buttonLabel}
-        </Button>
-      )}
+      {indicatorText && <StatusIndicator active={active} text={indicatorText} />}
     </div>
   );
 };
@@ -51,8 +50,6 @@ export default RecruitmentItem;
 
 RecruitmentItem.propTypes = {
   recruitment: PropTypes.object.isRequired,
-  buttonLabel: PropTypes.string,
-  isButtonDisabled: PropTypes.bool,
   onClickButton: PropTypes.func,
   className: PropTypes.string,
 };
