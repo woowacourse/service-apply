f8a9a79739e1ca7b7e02f2a6804e77845fec0f57
kth990303 <kth990303@naver.com>
2022-09-27 17:20:57 +0900

    refactor(administrator): use `PasswordField` to input password

--- src/main/kotlin/apply/ui/admin/administrator/AdministratorForm.kt
+++ src/main/kotlin/apply/ui/admin/administrator/AdministratorForm.kt
@@ -1,14 +1,15 @@
 package apply.ui.admin.administrator
 
 import apply.application.AdministratorData
+import com.vaadin.flow.component.textfield.PasswordField
 import com.vaadin.flow.component.textfield.TextField
 import support.views.BindingIdentityFormLayout
 
 class AdministratorForm : BindingIdentityFormLayout<AdministratorData>(AdministratorData::class) {
     private val name: TextField = TextField("관리자명")
     private val username: TextField = TextField("관리자 사용자명")
-    private val password: TextField = TextField("비밀번호")
-    private val confirmPassword: TextField = TextField("비밀번호 확인")
+    private val password: PasswordField = PasswordField("비밀번호")
+    private val confirmPassword: PasswordField = PasswordField("비밀번호 확인")
 
     init {
         add(name, username, password, confirmPassword)
