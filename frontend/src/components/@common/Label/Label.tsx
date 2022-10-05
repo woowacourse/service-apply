f126937b591df996a8676047fab1d057f6b65c4e
kimsanglok <evertree6031@gmail.com>
2022-10-05 19:14:36 +0900

    feat: Label - add htmlFor prop

--- frontend/src/components/@common/Label/Label.tsx
+++ frontend/src/components/@common/Label/Label.tsx
@@ -2,14 +2,16 @@ import classNames from "classnames";
 import styles from "./Label.module.css";
 
 export type LabelProps = {
+  htmlFor?: string;
   className?: string;
   required?: boolean;
   children: React.ReactNode;
 };
 
-const Label = ({ className = "", required = false, children }: LabelProps) => {
+const Label = ({ htmlFor, className = "", required = false, children }: LabelProps) => {
   return (
     <label
+      htmlFor={htmlFor}
       className={classNames(styles.label, className, {
         [styles.required]: required,
       })}
