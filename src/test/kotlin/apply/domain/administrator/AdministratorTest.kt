2638fb6b3ef01000f0cc4dd8996b25b1be234543
kth990303 <kth990303@naver.com>
2022-10-09 13:58:32 +0900

    test: remove unnecessary scope function in AdministratorTest

--- src/test/kotlin/apply/domain/administrator/AdministratorTest.kt
+++ src/test/kotlin/apply/domain/administrator/AdministratorTest.kt
@@ -7,12 +7,8 @@ import io.kotest.matchers.shouldBe
 
 class AdministratorTest : StringSpec({
     "관리자명과 비밀번호를 수정한다" {
-        val administrator = createAdministrator("케이", "kth990303", "1234").apply {
-            update(
-                name = "케이케이",
-                password = "12345"
-            )
-        }
+        val administrator = createAdministrator("케이", "kth990303", "1234")
+        administrator.update("케이케이", "12345")
         assertSoftly(administrator) {
             administrator.name shouldBe "케이케이"
             administrator.username shouldBe "kth990303"
