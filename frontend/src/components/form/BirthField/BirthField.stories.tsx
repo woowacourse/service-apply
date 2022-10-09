d81f603d32cdfdb4373aa9fd57034baa8a6e538b
kimsanglok <evertree6031@gmail.com>
2022-10-09 18:23:02 +0900

    refactor: migrate BirthField  file to typescript
    (cherry picked from commit 89e796b90507b64c14f0beb47c62f6b607274c38)

--- frontend/src/components/form/BirthField/BirthField.stories.tsx
+++ frontend/src/components/form/BirthField/BirthField.stories.tsx
@@ -1,15 +1,14 @@
-import React from "react";
+import { ComponentMeta, ComponentStory } from "@storybook/react";
 import BirthField from "./BirthField";
 
 export default {
   title: "form/BirthField",
   component: BirthField,
-};
+} as ComponentMeta<typeof BirthField>;
 
-const Template = (args) => <BirthField {...args} />;
+const Template: ComponentStory<typeof BirthField> = (args) => <BirthField {...args} />;
 
 export const Default = Template.bind({});
-
 export const Required = Template.bind({});
 Required.args = {
   required: true,
--- frontend/src/components/form/BirthField/BirthField.tsx
+++ frontend/src/components/form/BirthField/BirthField.tsx
@@ -1,14 +1,26 @@
+import DatePicker, { ReactDatePickerProps, registerLocale } from "react-datepicker";
 import classNames from "classnames";
 import ko from "date-fns/locale/ko";
-import PropTypes from "prop-types";
-import DatePicker, { registerLocale } from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+
 import Label from "../../@common/Label/Label";
+
 import styles from "./BirthField.module.css";
 
 registerLocale("ko", ko);
 
-const BirthField = ({ value, onChange, required, className, ...props }) => {
+type BirthFieldProps = Omit<ReactDatePickerProps, "value"> & {
+  value: Date | null;
+};
+
+const BirthField = ({
+  value = null,
+  onChange,
+  required = false,
+  className,
+  name,
+  ...props
+}: BirthFieldProps) => {
   return (
     <div className={classNames(styles.box, className)}>
       <div className={styles["input-box"]}>
@@ -34,20 +46,6 @@ const BirthField = ({ value, onChange, required, className, ...props }) => {
       </div>
     </div>
   );
-};
-
-BirthField.propTypes = {
-  value: PropTypes.instanceOf(Date),
-  onChange: PropTypes.func,
-  errorMessage: PropTypes.string,
-  required: PropTypes.bool,
-
-  className: PropTypes.string,
-};
-
-BirthField.defaultProps = {
-  value: null,
-  required: false,
 };
 
 export default BirthField;
