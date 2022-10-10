3ac8aa6aecc6b2953f84daaf27d0c5b1d8e6317c
Jeongmin Woo <greenblues1190@gmail.com>
2022-10-10 16:17:04 +0900

    refactor: change interface to type alias
    Refs: https://github.com/woowacourse/service-apply/pull/649#discussion_r988253699

--- frontend/src/components/Modal/ModalWindow/ModalWindow.tsx
+++ frontend/src/components/Modal/ModalWindow/ModalWindow.tsx
@@ -1,10 +1,9 @@
 import classNames from "classnames";
 import styles from "./ModalWindow.module.css";
 
-interface ModalWindowProps
-  extends Omit<React.HTMLAttributes<HTMLDivElement>, "role" | "aria-label"> {
+type ModalWindowProps = Omit<React.HTMLAttributes<HTMLDivElement>, "role" | "aria-label"> & {
   title: string;
-}
+};
 
 const ModalWindow = ({ className, title, children, ...props }: ModalWindowProps) => {
   return (
