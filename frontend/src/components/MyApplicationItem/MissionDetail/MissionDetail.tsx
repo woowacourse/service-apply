7f65aa94ed4f50bb3bdb6d8cfb596c4913476be4
Yunho <ansghkdbsgh@gmail.com>
2022-10-13 00:49:44 +0900

    feat: 예제테스트 실행에 대한 PR주소를 보여주지 않도록 수정

--- frontend/src/components/MyApplicationItem/MissionDetail/MissionDetail.tsx
+++ frontend/src/components/MyApplicationItem/MissionDetail/MissionDetail.tsx
@@ -3,7 +3,6 @@ import { MY_MISSION_TOOLTIP_MESSAGE } from "../../../constants/messages";
 import Tooltip from "../../@common/Tooltip/Tooltip";
 import CommitHash from "../CommitHash/CommitHash";
 import JudgmentResultText from "../JudgmentResult/JudgmentResult";
-import PullRequestUrl from "../PullRequestUrl/PullRequestUrl";
 import styles from "./MissionDetail.module.css";
 
 type MissionDetailProps = {
@@ -14,7 +13,6 @@ const MissionDetail = ({ judgment }: MissionDetailProps) => {
   return (
     <div className={styles["detail-container"]}>
       <JudgmentResultText judgment={judgment} />
-      <PullRequestUrl judgment={judgment} />
       <CommitHash judgment={judgment} />
       <div className={styles["guide-container"]}>
         <p>테스트 코드 실행이 끝나기까지 3 ~ 5분이 걸릴 수 있습니다</p>
