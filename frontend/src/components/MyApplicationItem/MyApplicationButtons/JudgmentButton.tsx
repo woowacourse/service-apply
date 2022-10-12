deb64f0519f8479eb2ef9d9e4d320a2861426aeb
Yunho <ansghkdbsgh@gmail.com>
2022-10-12 19:39:59 +0900

    refactor: judgebutton 실행 실패시 에러처리 변경
    Co-authored-by: Jungmin Hwang <hwangstar156@users.noreply.github.com>

--- frontend/src/components/MyApplicationItem/MyApplicationButtons/JudgmentButton.tsx
+++ frontend/src/components/MyApplicationItem/MyApplicationButtons/JudgmentButton.tsx
@@ -1,7 +1,7 @@
 import { AxiosError } from "axios";
 import classNames from "classnames";
 import { Mission, Recruitment } from "../../../../types/domains/recruitments";
-import { fetchMyMissionJudgment, postMyMissionJudgment } from "../../../api/recruitments";
+import { postMyMissionJudgment } from "../../../api/recruitments";
 import { JUDGMENT_STATUS } from "../../../constants/judgment";
 import { MISSION_STATUS } from "../../../constants/recruitment";
 import useTokenContext from "../../../hooks/useTokenContext";
@@ -25,13 +25,6 @@ const JudgmentButton = ({ missionItem, recruitmentId, setMission }: JudgmentButt
 
     const errorMessage = error.response?.data.message;
     alert(errorMessage);
-
-    const response = await fetchMyMissionJudgment({
-      recruitmentId: Number(recruitmentId),
-      missionId: Number(missionId),
-      token,
-    });
-    setMission({ ...missionItem, judgment: response.data });
   };
 
   const handleJudgeMission = async ({
