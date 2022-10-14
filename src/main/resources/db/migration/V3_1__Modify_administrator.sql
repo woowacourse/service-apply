a17fe1eac37af343450fee5b1438b5babc20df06
박재성 <jason@woowahan.com>
2022-10-14 15:10:07 +0900

    build(apply): set the time zone and prepare for deployment

--- src/main/resources/db/migration/V3_1__Modify_administrator.sql
+++
@@ -1,5 +0,0 @@
-alter table administrator
-    modify name varchar(30) not null,
-    add username varchar(30) not null after password,
-    add constraint uk_administrator unique (username),
-    drop index `UK_qv926x9u07ru5erc4vn3lxhcg`;
