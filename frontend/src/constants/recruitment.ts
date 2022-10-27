bb0f6085c344966dbefc7693b2df425e233fe7fe
Yunho <ansghkdbsgh@gmail.com>
2022-10-27 20:13:43 +0900

    refactor: BUTTON_LABEL 을 const/recruitment 로 이동

--- frontend/src/constants/recruitment.ts
+++ frontend/src/constants/recruitment.ts
@@ -11,3 +11,12 @@ export const MISSION_STATUS = {
   UNSUBMITTABLE: "UNSUBMITTABLE",
   ENDED: "ENDED",
 } as const;
+
+export const BUTTON_LABEL = {
+  BEFORE_SUBMISSION: "준비 중",
+  EDIT: "수정하기",
+  SUBMIT: "제출하기",
+  UNSUBMITTABLE: "제출불가",
+  COMPLETE: "제출완료",
+  UNSUBMITTED: "미제출",
+} as const;
