108b41cc0fae4c3b5ba49c4db0a3a7ce3e2f0f8e
kimsanglok <evertree6031@gmail.com>
2022-11-22 20:08:12 +0900

    refactor: remove annotation

--- frontend/src/date-picker.d.ts
+++ frontend/src/date-picker.d.ts
@@ -1,14 +1,5 @@
 import "react-datepicker";
 
-/**
- * 선언병합은 왜 하였는가?
- * 1. react-datepicker는 BirthField 컴포넌트와 관련이 있다.
- * 2. BirthField의 props 중에서는 onChange가 존재한다.
- * 2. BirthField는 ReactDatePickerProps 타입을 상속받는다.
- * 3. ReactDatePickerProps 타입에서는 onChange가 optional이 아니다.
- * 4. BirthField의 onChange의 타입은 optional이어야 한다.
- * 5. 그렇기에 선언병합을 통해서 onChange 타입에 optional을 추가하였다.
- */
 declare module "react-datepicker" {
   export interface ReactDatePickerProps {
     onChange?(
