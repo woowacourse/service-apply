12377f1ef653110d9f5188f64314558fbc0c3da3
woowahan-neo <jaeyeonling@woowahan.com>
2023-05-09 16:44:39 +0900

    배정된 평가 대상자, 전체 평가 대상자 탭 생성

--- src/main/kotlin/apply/ui/admin/selections/SelectionMeView.kt
+++ src/main/kotlin/apply/ui/admin/selections/SelectionMeView.kt
@@ -3,6 +3,7 @@ package apply.ui.admin.selections
 import apply.application.RecruitmentService
 import apply.ui.admin.BaseLayout
 import com.vaadin.flow.component.Component
+import com.vaadin.flow.component.UI
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
@@ -11,6 +12,7 @@ import com.vaadin.flow.router.BeforeEvent
 import com.vaadin.flow.router.HasUrlParameter
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.router.WildcardParameter
+import support.views.createNormalButton
 
 @Route(value = "admin/selections/me", layout = BaseLayout::class)
 class SelectionMeView(
@@ -23,13 +25,25 @@ class SelectionMeView(
         this.recruitmentId = parameter
         modelView["title"] = recruitmentService.getById(recruitmentId).title
 
-        add(createTitle(), createContent())
+        add(createTitle(), createEvaluationTargetTabs(), createContent())
     }
 
     private fun createTitle(): Component {
         return HorizontalLayout(H1(modelView["title"] as String)).apply {
             setWidthFull()
             justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+        }
+    }
+
+    private fun createEvaluationTargetTabs(): Component {
+        return HorizontalLayout(
+            createNormalButton("배정된 평가 대상자") { },
+            createNormalButton("전체 평가 대상자") {
+                UI.getCurrent().navigate(SelectionView::class.java, recruitmentId)
+            }
+        ).apply {
+            setWidthFull()
+            justifyContentMode = FlexComponent.JustifyContentMode.START
         }
     }
 
