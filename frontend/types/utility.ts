5358d8612f74a53bb8cceffeda4b470a59979845
Soyi Jeon <soyi.dev@gmail.com>
2023-05-09 15:53:22 +0900

    refactor(apply): remove unnecessary server response type

--- frontend/types/utility.ts
+++ frontend/types/utility.ts
@@ -1,8 +1,3 @@
 export type RequestWithToken<T = {}> = { token: string } & T;
 
-export type ResponseDataWithMessage<T> = {
-  message: string;
-  body: T;
-};
-
 export type ValueOf<T> = T[keyof T];
