938661715544b0557ed333c18780222b9e6581b2
woowahan-neo <jaeyeonling@woowahan.com>
2023-05-11 16:01:21 +0900

    평가 대상자 테이블, 평가 다운로드, 평가 업로드 버튼 추가

--- src/main/kotlin/apply/ui/admin/selections/SelectionMeView.kt
+++ src/main/kotlin/apply/ui/admin/selections/SelectionMeView.kt
@@ -6,19 +6,37 @@ import apply.application.RecruitmentService
 import apply.ui.admin.BaseLayout
 import com.vaadin.flow.component.Component
 import com.vaadin.flow.component.UI
+import com.vaadin.flow.component.button.Button
+import com.vaadin.flow.component.grid.Grid
 import com.vaadin.flow.component.html.H1
 import com.vaadin.flow.component.orderedlayout.FlexComponent
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout
 import com.vaadin.flow.component.orderedlayout.VerticalLayout
 import com.vaadin.flow.component.tabs.Tab
 import com.vaadin.flow.component.tabs.Tabs
+import com.vaadin.flow.component.upload.Upload
+import com.vaadin.flow.component.upload.receivers.MemoryBuffer
+import com.vaadin.flow.data.renderer.ComponentRenderer
+import com.vaadin.flow.data.renderer.Renderer
 import com.vaadin.flow.router.BeforeEvent
 import com.vaadin.flow.router.HasUrlParameter
 import com.vaadin.flow.router.Route
 import com.vaadin.flow.router.WildcardParameter
+import support.views.addSortableColumn
+import support.views.createCsvUpload
 import support.views.createErrorButton
 import support.views.createNormalButton
+import support.views.createPrimarySmallButton
 import support.views.createSearchBox
+import support.views.createSuccessButton
+
+data class MyEvaluationTargetResponse(
+    val name: String,
+    val email: String,
+    val totalScore: Double,
+    val totalStatus: String,
+    val myStatus: String
+)
 
 @Route(value = "admin/selections/me", layout = BaseLayout::class)
 class SelectionMeView(
@@ -32,7 +50,13 @@ class SelectionMeView(
         recruitmentId = parameter
         modelView["title"] = recruitmentService.getById(recruitmentId).title
         modelView["evaluations"] = evaluationService.getAllSelectDataByRecruitmentId(recruitmentId)
-        add(createTitle(), createEvaluationTargetTabs(), createMenu(), createContent())
+        modelView["contents"] = listOf(
+            MyEvaluationTargetResponse("홍길동1", "test1@test.com", 7.0, "평가 전", "평가 전"),
+            MyEvaluationTargetResponse("홍길동2", "test2@test.com", 7.8, "합격", "합격"),
+            MyEvaluationTargetResponse("홍길동3", "test3@test.com", 9.1, "합격", "탈락")
+        )
+
+        add(createTitle(), createEvaluationTargetTabs(), createMenu(), createContent(), createEvaluationTargetFileButtons())
     }
 
     private fun createTitle(): Component {
@@ -83,9 +107,47 @@ class SelectionMeView(
     }
 
     private fun createContent(): Component {
-        return HorizontalLayout().apply {
+        val contents = modelView["contents"] as List<MyEvaluationTargetResponse>
+        return Grid<MyEvaluationTargetResponse>(10).apply {
+            addSortableColumn("이름", MyEvaluationTargetResponse::name)
+            addSortableColumn("이메일", MyEvaluationTargetResponse::email)
+            addSortableColumn("종합 점수", MyEvaluationTargetResponse::totalScore)
+            addSortableColumn("종합 평가 상태", MyEvaluationTargetResponse::totalStatus)
+            addSortableColumn("나의 평가 상태", MyEvaluationTargetResponse::myStatus)
+            addColumn(createEvaluationButtonRenderer()).apply {
+                isAutoWidth = true
+                setHeader("평가")
+            }
+            setItems(contents)
+        }
+    }
+
+    private fun createEvaluationButtonRenderer(): Renderer<MyEvaluationTargetResponse> {
+        return ComponentRenderer { response ->
+            createPrimarySmallButton("평가하기") {
+                // TODO 평가하기
+            }
+        }
+    }
+
+    private fun createEvaluationTargetFileButtons(): Component {
+        return HorizontalLayout(
+            createEvaluationFileDownloadButton(),
+            createEvaluationFileUpload()
+        ).apply {
             setWidthFull()
             justifyContentMode = FlexComponent.JustifyContentMode.CENTER
+            defaultVerticalComponentAlignment = FlexComponent.Alignment.CENTER
+        }
+    }
+
+    private fun createEvaluationFileDownloadButton(): Button {
+        return createSuccessButton("평가지 다운로드") {
+        }
+    }
+
+    private fun createEvaluationFileUpload(): Upload {
+        return createCsvUpload("평가지 업로드", MemoryBuffer()) {
         }
     }
 }
