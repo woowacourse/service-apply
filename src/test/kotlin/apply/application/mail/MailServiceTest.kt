25a84efccfb1c505e4d62bc4c636e02804b49ede
woowabrie <buyong.jo@woowahan.com>
2023-09-07 14:26:28 +0900

    test: use multiline strings for test data and enhance test match

--- src/test/kotlin/apply/application/mail/MailServiceTest.kt
+++ src/test/kotlin/apply/application/mail/MailServiceTest.kt
@@ -14,16 +14,20 @@ class MailServiceTest(
 ) : BehaviorSpec({
 
     Given("MailData를 사용해서 메일을 보내는 경우") {
-        val body = "안녕하세요. 우아한테크코스입니다.\n" + "\n" +
-            "1주 차 미션은 개발 환경을 세팅하고, GitHub에 과제를 제출하는 등 미션 외에도 추가로 익혀야 하는 부분들이 있어 가벼운 미션으로 준비했어요."
+        val body = """
+            안녕하세요. 우아한테크코스입니다.
+            
+            1주 차 미션은 개발 환경을 세팅하고, GitHub에 과제를 제출하는 등 미션 외에도 추가로 익혀야 하는 부분들이 있어 가벼운 미션으로 준비했어요.
+        """.trimIndent()
         val mailData = createMailData(body = body)
 
         When("common 메일 본문에 적용하면") {
             val commonMailBody = mailService.toHtmlBody(mailData)
 
             Then("본문이 포함된 HTML로 작성된다.") {
-                println(commonMailBody)
-                commonMailBody shouldContain "<body><p>안녕하세요. 우아한테크코스입니다."
+                commonMailBody shouldContain "<title>email</title>"
+                commonMailBody shouldContain "<body><p>안녕하세요. 우아한테크코스입니다.</p>"
+                commonMailBody shouldContain "</html>"
             }
         }
     }
--- src/test/kotlin/support/MarkdownTest.kt
+++ src/test/kotlin/support/MarkdownTest.kt
@@ -5,25 +5,29 @@ import io.kotest.matchers.string.shouldContain
 
 class MarkdownTest : StringSpec({
     "마크다운 문법으로 작성된 내용을 HTML로 변환할 수 있다" {
-        val src = "안녕하세요. 우아한테크코스입니다.\n" +
-            "3주 차 미션에서는 2주 차에서 학습한 것에 2가지 목표를 추가했어요.\n" +
-            "1. 클래스(객체)를 분리하는 연습\n" +
-            "2. 도메인 로직에 대한 단위 테스트를 작성하는 연습\n" +
-            "## 프리코스 진행방식\n" +
-            "### 미션 제출 방법\n" +
-            "- 미션 구현을 완료한 후 GitHub을 통해 제출해야 한다.\n" +
-            "- GitHub을 활용한 제출 방법은 " +
-            "[문서](https://github.com/woowacourse/woowacourse-docs/tree/master/precourse)를 참고해 제출한다.\n" +
-            "- GitHub에 미션을 제출한 후 " +
-            "[우아한테크코스 지원 플랫폼](https://apply.techcourse.co.kr/)에서 프리코스 과제를 제출한다.\n" +
-            "- 지원 플랫폼에서 과제를 제출할 때 미션을 진행하며 경험한 내용에 대한 소감문을 작성한다."
+        val src = """
+            안녕하세요. 우아한테크코스입니다.
+
+            3주 차 미션에서는 2주 차에서 학습한 것에 2가지 목표를 추가했어요.
+
+            1. 클래스(객체)를 분리하는 연습
+            2. 도메인 로직에 대한 단위 테스트를 작성하는 연습
+            
+            ## 프리코스 진행방식
+
+            ### 미션 제출 방법
+            
+            - 미션 구현을 완료한 후 GitHub을 통해 제출해야 한다.
+            - GitHub을 활용한 제출 방법은 [문서](https://github.com/woowacourse/woowacourse-docs/tree/master/precourse)를 참고해 제출한다.
+            - GitHub에 미션을 제출한 후 [우아한테크코스 지원 플랫폼](https://apply.techcourse.co.kr/)에서 프리코스 과제를 제출한다.
+            - 지원 플랫폼에서 과제를 제출할 때 미션을 진행하며 경험한 내용에 대한 소감문을 작성한다.
+        """.trimIndent()
 
         val html = markdownToHtml(src)
 
-        println(html)
-        html shouldContain "<ul>"
-        html shouldContain "<li>클래스(객체)를 분리하는 연습</li>"
-        html shouldContain "<h3>"
-        html shouldContain "<a href"
+        html shouldContain "<ol><li>클래스(객체)를 분리하는 연습</li><li>도메인 로직에 대한 단위 테스트를 작성하는 연습</li></ol>"
+        html shouldContain "<h2>프리코스 진행방식</h2>"
+        html shouldContain "<h3>미션 제출 방법</h3>"
+        html shouldContain "<li>GitHub에 미션을 제출한 후 <a href=\"https://apply.techcourse.co.kr/\">우아한테크코스 지원 플랫폼</a>에서 프리코스 과제를 제출한다.</li>"
     }
 })
