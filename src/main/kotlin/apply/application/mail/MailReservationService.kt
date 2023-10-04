9e29d51bbb9f868aed423c0cb6085794f1072821
woowabrie <buyong.jo@woowahan.com>
2023-10-05 08:35:47 +0900

    feat: add functionality for finding mail's reservation

--- src/main/kotlin/apply/application/mail/MailReservationService.kt
+++ src/main/kotlin/apply/application/mail/MailReservationService.kt
@@ -1,6 +1,7 @@
 package apply.application.mail
 
 import apply.domain.mail.MailReservationRepository
+import apply.domain.mail.MailReservationStatus
 import apply.domain.mail.getOrThrow
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -11,6 +12,11 @@ import java.time.LocalDateTime
 class MailReservationService(
     private val mailReservationRepository: MailReservationRepository,
 ) {
+    fun findByWaitingStatus(): List<MailReservationResponse> {
+        return mailReservationRepository.findByStatus(MailReservationStatus.WAITING)
+            .map { MailReservationResponse(it) }
+    }
+
     fun updateReservationTime(mailReservationId: Long, reservationTime: LocalDateTime): MailReservationResponse {
         val mailReservation = mailReservationRepository.getOrThrow(mailReservationId).apply {
             update(reservationTime)
--- src/main/kotlin/apply/domain/mail/MailReservationRepository.kt
+++ src/main/kotlin/apply/domain/mail/MailReservationRepository.kt
@@ -6,4 +6,6 @@ import org.springframework.data.repository.findByIdOrNull
 fun MailReservationRepository.getOrThrow(id: Long) = findByIdOrNull(id)
     ?: throw NoSuchElementException("메일 예약이 존재하지 않습니다. id: $id")
 
-interface MailReservationRepository : JpaRepository<MailReservation, Long>
+interface MailReservationRepository : JpaRepository<MailReservation, Long> {
+    fun findByStatus(status: MailReservationStatus): List<MailReservation>
+}
