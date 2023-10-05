f766d5def0c437487650d1ef2625b3cc054b3493
woowabrie <buyong.jo@woowahan.com>
2023-10-05 15:45:03 +0900

    refactor: migrate sql relating with mail tables

--- src/main/resources/db/migration/V3_3__rename_mail_history.sql
+++
@@ -1,3 +0,0 @@
-rename table mail_history to mail_history_old;
-
-rename table mail_history2 to mail_history;
--- src/main/resources/db/migration/V4__Create_mail_tables.sql
+++ src/main/resources/db/migration/V4__Create_mail_tables.sql
@@ -27,3 +27,21 @@ alter table mail_reservation
     add constraint fk_mail_reservation_to_mail_message
     foreign key (mail_message_id)
     references mail_message (id);
+
+rename table mail_history to mail_history_old;
+
+create table mail_history
+(
+    id         bigint       not null auto_increment,
+    recipients longtext     not null,
+    sent_time  datetime(6)  not null,
+    success    bit(1)       not null,
+    mail_message_id bigint  not null,
+    primary key (id)
+) engine = InnoDB
+  default charset = utf8mb4;
+
+alter table mail_history
+    add constraint fk_mail_history_to_mail_message
+    foreign key (mail_message_id)
+    references mail_message (id);
