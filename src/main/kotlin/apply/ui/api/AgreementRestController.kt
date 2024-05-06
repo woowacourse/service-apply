a39f54e9b38ffd2ada98f5d8c7a487deef9e2c6e
woowahan-pjs <jason@woowahan.com>
2024-05-06 22:28:23 +0900

    fix(agreement): change em to rem

--- src/main/kotlin/apply/ui/api/AgreementRestController.kt
+++ src/main/kotlin/apply/ui/api/AgreementRestController.kt
@@ -18,7 +18,7 @@ class AgreementRestController {
             """
                 (주)우아한형제들은 아래와 같이 지원자의 개인정보를 수집 및 이용합니다.
                 
-                <strong>보유 및 이용기간</strong> : <strong><span style="font-size:1.2em">탈퇴 시 또는 이용목적 달성 시 파기</span></strong>(단, 관련법령 및 회사정책에 의해 보관이 필요한 경우 해당기간 동안 보관)
+                <strong>보유 및 이용기간</strong> : <strong><span style="font-size:1.2rem">탈퇴 시 또는 이용목적 달성 시 파기</span></strong>(단, 관련법령 및 회사정책에 의해 보관이 필요한 경우 해당기간 동안 보관)
             """.trimIndent()
         )
         return ResponseEntity.ok(ApiResponse.success(response))
