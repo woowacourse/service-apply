6f417f9783bf21c21d6ca3ed6b89e127a83ec3b6
Cron <hojoong.choi@woowahan.com>
2024-07-02 11:29:22 +0900

    refactor: MyMissionItem 훅 분리

--- frontend/src/components/MyApplicationItem/MyMissionItem/MyMissionItem.tsx
+++ frontend/src/components/MyApplicationItem/MyMissionItem/MyMissionItem.tsx
@@ -1,104 +1,33 @@
 import classNames from "classnames";
-import { useEffect, useMemo, useState } from "react";
-import { generatePath } from "react-router";
-import { useNavigate } from "react-router-dom";
 import { Mission } from "../../../../types/domains/recruitments";
-import { PARAM } from "../../../constants/path";
 import { BUTTON_LABEL, MISSION_STATUS } from "../../../constants/recruitment";
-import { formatDateTime } from "../../../utils/format/date";
 import MissionDetail from "../MissionDetail/MissionDetail";
 import ApplyButton from "../MyApplicationButtons/ApplyButton";
 import JudgmentButton from "../MyApplicationButtons/JudgmentButton";
 import RefreshButton from "../MyApplicationButtons/RefreshButton";
 import styles from "../MyApplicationItem.module.css";
 import RecruitmentDetail from "../RecruitmentDetail/RecruitmentDetail";
-import { PATH } from "./../../../constants/path";
-import useRefresh from "./useRefresh";
-import useMissionJudgement from "./useMissionJudgement";
+import useMission from "./useMission";
 
 type MyMissionItemProps = {
   mission: Mission;
   recruitmentId: string;
 };
 
-const missionLabel = (submitted: boolean, missionStatus: Mission["status"]) => {
-  const labelMap = {
-    SUBMITTABLE: BUTTON_LABEL.BEFORE_SUBMIT,
-    SUBMITTING: submitted ? BUTTON_LABEL.EDIT : BUTTON_LABEL.SUBMIT,
-    UNSUBMITTABLE: BUTTON_LABEL.UNSUBMITTABLE,
-    ENDED: submitted ? BUTTON_LABEL.COMPLETE : BUTTON_LABEL.UNSUBMITTED,
-  } as const;
-
-  return labelMap[missionStatus];
-};
-
 const MyMissionItem = ({ mission, recruitmentId }: MyMissionItemProps) => {
-  const navigate = useNavigate();
-
-  const [missionItem, setMissionItem] = useState<Mission>({ ...mission });
-  const { refreshAvailable, fetchRefreshedResultData } = useRefresh({
-    missionItem,
-    recruitmentId: Number(recruitmentId),
-  });
-  const { fetchJudgmentMissionResult, isJudgmentAvailable } = useMissionJudgement({
-    missionItem,
-    recruitmentId: Number(recruitmentId),
-  });
-
-  const applyButtonLabel = missionLabel(mission.submitted, mission.status);
-
-  useEffect(() => {
-    setMissionItem(mission);
-  }, [mission]);
-
-  const formattedStartDateTime = useMemo(
-    () => (missionItem.startDateTime ? formatDateTime(new Date(missionItem.startDateTime)) : ""),
-    [missionItem.startDateTime]
-  );
-
-  const formattedEndDateTime = useMemo(
-    () => (missionItem.endDateTime ? formatDateTime(new Date(missionItem.endDateTime)) : ""),
-    [missionItem.endDateTime]
-  );
-
-  const routeToAssignmentSubmit =
-    ({ recruitmentId, mission }: { recruitmentId: string; mission: Mission }) =>
-    () => {
-      const isSubmitted = mission.submitted;
-
-      navigate(
-        {
-          pathname: generatePath(PATH.ASSIGNMENT, {
-            status: isSubmitted ? PARAM.ASSIGNMENT_STATUS.EDIT : PARAM.ASSIGNMENT_STATUS.NEW,
-          }),
-        },
-        {
-          state: {
-            recruitmentId,
-            currentMission: mission,
-          },
-        }
-      );
-    };
-
-  const requestRefresh = async () => {
-    try {
-      const result = await fetchRefreshedResultData();
-      setMissionItem(result);
-    } catch (error) {
-      error instanceof Error && alert(error.message);
-    }
-  };
-
-  const requestMissionJudgment = async () => {
-    try {
-      const result = await fetchJudgmentMissionResult();
-
-      setMissionItem(result);
-    } catch (error) {
-      error instanceof Error && alert(error.message);
-    }
-  };
+  const {
+    getter: {
+      missionItem,
+      applyButtonLabel,
+      formattedStartDateTime,
+      formattedEndDateTime,
+      isJudgmentAvailable,
+      refreshAvailable,
+    },
+    routeToAssignmentSubmit,
+    requestRefresh,
+    requestMissionJudgment,
+  } = useMission({ mission, recruitmentId });
 
   return (
     <div className={classNames(styles["content-box"])}>
---
+++ frontend/src/components/MyApplicationItem/MyMissionItem/useMission.ts
@@ -0,0 +1,112 @@
+import { useEffect, useMemo, useState } from "react";
+import { formatDateTime } from "../../../utils/format/date";
+import useMissionJudgement from "./useMissionJudgement";
+import { generatePath, useNavigate } from "react-router-dom";
+import useRefresh from "./useRefresh";
+import { Mission } from "../../../../types/domains/recruitments";
+import { PARAM, PATH } from "../../../constants/path";
+import { BUTTON_LABEL } from "../../../constants/recruitment";
+
+type MissionProps = {
+  mission: Mission;
+  recruitmentId: string;
+};
+
+const missionLabel = (submitted: boolean, missionStatus: Mission["status"]) => {
+  const labelMap = {
+    SUBMITTABLE: BUTTON_LABEL.BEFORE_SUBMIT,
+    SUBMITTING: submitted ? BUTTON_LABEL.EDIT : BUTTON_LABEL.SUBMIT,
+    UNSUBMITTABLE: BUTTON_LABEL.UNSUBMITTABLE,
+    ENDED: submitted ? BUTTON_LABEL.COMPLETE : BUTTON_LABEL.UNSUBMITTED,
+  } as const;
+
+  return labelMap[missionStatus];
+};
+
+const useMission = ({ mission, recruitmentId }: MissionProps) => {
+  const navigate = useNavigate();
+
+  const [missionItem, setMissionItem] = useState<Mission>({ ...mission });
+  const { refreshAvailable, fetchRefreshedResultData } = useRefresh({
+    missionItem,
+    recruitmentId: Number(recruitmentId),
+  });
+  const { fetchJudgmentMissionResult, isJudgmentAvailable } = useMissionJudgement({
+    missionItem,
+    recruitmentId: Number(recruitmentId),
+  });
+
+  const applyButtonLabel = missionLabel(mission.submitted, mission.status);
+
+  useEffect(() => {
+    setMissionItem(mission);
+  }, [mission]);
+
+  const formattedStartDateTime = useMemo(
+    () => (missionItem.startDateTime ? formatDateTime(new Date(missionItem.startDateTime)) : ""),
+    [missionItem.startDateTime]
+  );
+
+  const formattedEndDateTime = useMemo(
+    () => (missionItem.endDateTime ? formatDateTime(new Date(missionItem.endDateTime)) : ""),
+    [missionItem.endDateTime]
+  );
+
+  const routeToAssignmentSubmit =
+    ({ recruitmentId, mission }: { recruitmentId: string; mission: Mission }) =>
+    () => {
+      const isSubmitted = mission.submitted;
+
+      navigate(
+        {
+          pathname: generatePath(PATH.ASSIGNMENT, {
+            status: isSubmitted ? PARAM.ASSIGNMENT_STATUS.EDIT : PARAM.ASSIGNMENT_STATUS.NEW,
+          }),
+        },
+        {
+          state: {
+            recruitmentId,
+            currentMission: mission,
+          },
+        }
+      );
+    };
+
+  const requestRefresh = async () => {
+    try {
+      const result = await fetchRefreshedResultData();
+      setMissionItem(result);
+    } catch (error) {
+      error instanceof Error && alert(error.message);
+    }
+  };
+
+  const requestMissionJudgment = async () => {
+    try {
+      const result = await fetchJudgmentMissionResult();
+
+      setMissionItem(result);
+    } catch (error) {
+      error instanceof Error && alert(error.message);
+    }
+  };
+
+  return {
+    getter: {
+      missionItem,
+      applyButtonLabel,
+      formattedStartDateTime,
+      formattedEndDateTime,
+      isJudgmentAvailable,
+      refreshAvailable,
+    },
+    setter: {
+      setMissionItem,
+    },
+    routeToAssignmentSubmit,
+    requestRefresh,
+    requestMissionJudgment,
+  };
+};
+
+export default useMission;
