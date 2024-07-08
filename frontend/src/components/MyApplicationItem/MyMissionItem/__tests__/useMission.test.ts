c53dd81bb68830c7cc59a1c389577e4e9e13e83e
Cron <hojoong.choi@woowahan.com>
2024-07-08 15:55:05 +0900

    refactor: useMission.test 코드 리뷰 반영하여 리팩터링

--- frontend/src/components/MyApplicationItem/MyMissionItem/__tests__/useMission.test.ts
+++ frontend/src/components/MyApplicationItem/MyMissionItem/__tests__/useMission.test.ts
@@ -14,56 +14,71 @@ jest.mock("react-router-dom", () => ({
   useNavigate: jest.fn(),
 }));
 
-describe("useMission", () => {
-  const mockMission: Mission = {
-    id: 1,
-    title: "Test Mission",
-    description: "Test Description",
-    submittable: true,
-    submitted: false,
-    startDateTime: "2023-01-01T00:00:00",
-    endDateTime: "2023-12-31T23:59:59",
-    status: "SUBMITTING",
-    runnable: true,
-    judgment: null,
-  };
+const MOCK_MISSION_ITEM: Mission = {
+  id: 1,
+  title: "Test Mission",
+  description: "Test Description",
+  submittable: true,
+  submitted: false,
+  startDateTime: "2023-01-01T00:00:00",
+  endDateTime: "2023-12-31T23:59:59",
+  status: "SUBMITTING",
+  runnable: true,
+  judgment: null,
+} as const;
 
+describe("useMission", () => {
   const mockRecruitmentId = "123";
+
+  function createMockMission(overrides = {}) {
+    return {
+      ...MOCK_MISSION_ITEM,
+      ...overrides,
+    };
+  }
 
   beforeEach(() => {
     (useRefresh as jest.Mock).mockReturnValue({
       isRefreshAvailable: true,
       fetchRefreshedResultData: jest
         .fn()
-        .mockResolvedValue({ ...mockMission, title: "Refreshed Mission" }),
+        .mockResolvedValue({ ...createMockMission(), title: "Refreshed Mission" }),
     });
     (useMissionJudgment as jest.Mock).mockReturnValue({
       isJudgmentAvailable: true,
       fetchJudgmentMissionResult: jest
         .fn()
-        .mockResolvedValue({ ...mockMission, title: "Judged Mission" }),
+        .mockResolvedValue({ ...createMockMission(), title: "Judged Mission" }),
     });
     (useNavigate as jest.Mock).mockReturnValue(jest.fn());
     (generatePath as jest.Mock).mockImplementation((path) => path);
   });
 
   describe("초기 상태 및 계산된 값을 점검", () => {
-    it("missionItem, 버튼 라벨, 포맷된 날짜, 그리고 가용성 플래그를 올바르게 설정해야 한다", () => {
+    const mockMission = createMockMission();
+
+    it("missionItem, 버튼 레이블, 날짜, flag 값을 정해진 형식대로 설정해야 한다", () => {
       const { result } = renderHook(() =>
         useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
       );
 
-      expect(result.current.getter.missionItem).toEqual(mockMission);
-      expect(result.current.getter.applyButtonLabel).toBe(BUTTON_LABEL.SUBMIT);
-      expect(result.current.getter.formattedStartDateTime).toBe("2023-01-01 00:00");
-      expect(result.current.getter.formattedEndDateTime).toBe("2023-12-31 23:59");
-      expect(result.current.getter.isJudgmentAvailable).toBe(true);
-      expect(result.current.getter.isRefreshAvailable).toBe(true);
+      expect(result.current.getter).toEqual(
+        expect.objectContaining({
+          missionItem: expect.objectContaining({ ...mockMission }),
+          applyButtonLabel: BUTTON_LABEL.SUBMIT,
+          formattedStartDateTime: "2023-01-01 00:00",
+          formattedEndDateTime: "2023-12-31 23:59",
+          isJudgmentAvailable: true,
+          isRefreshAvailable: true,
+        })
+      );
     });
   });
 
-  describe("mission prop 변경 시 동작을 테스트", () => {
-    it("mission prop이 변경되면 missionItem 상태를 새로운 값으로 업데이트해야 한다", () => {
+  describe("Mission props를 통한 상태 업데이트", () => {
+    const mockMission = createMockMission();
+
+    it("missionItem 상태를 새로운 값으로 갱신해야 한다", () => {
       const { result, rerender } = renderHook(
         ({ mission }) => useMission({ mission, recruitmentId: mockRecruitmentId }),
         { initialProps: { mission: mockMission } }
@@ -76,8 +91,9 @@ describe("useMission", () => {
     });
   });
 
-  describe("routeToAssignmentSubmit 함수를 테스트", () => {
-    it("미션이 제출되지 않았을 때, 'new' 상태로 과제 제출 페이지로 이동해야 한다", () => {
+  describe("라우팅 테스트", () => {
+    const mockMission = createMockMission();
+    it("미션 과제제출물 미제출 시, 'new' 상태로 과제 제출 페이지로 이동해야 한다", () => {
       const mockNavigate = jest.fn();
       (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
 
@@ -106,7 +122,7 @@ describe("useMission", () => {
       );
     });
 
-    it("미션이 이미 제출되었을 때, 'edit' 상태로 과제 제출 페이지로 이동해야 한다", () => {
+    it("미션 과제제출물 제출 시, 'edit' 상태로 과제 제출 페이지로 이동해야 한다", () => {
       const mockNavigate = jest.fn();
       (useNavigate as jest.Mock).mockReturnValue(mockNavigate);
 
@@ -136,74 +152,77 @@ describe("useMission", () => {
       );
     });
   });
+  describe("데이터 갱신 테스트", () => {
+    describe("requestRefresh 함수 테스트", () => {
+      const mockMission = createMockMission();
 
-  describe("requestRefresh 함수 테스트", () => {
-    it("fetchRefreshedResultData를 호출하고 그 결과로 missionItem을 업데이트해야 한다", async () => {
-      const { result } = renderHook(() =>
-        useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
-      );
+      it("fetchRefreshedResultData 성공 시 missionItem 상태를 갱신해야 한다", async () => {
+        const { result } = renderHook(() =>
+          useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
+        );
 
-      await act(async () => {
-        await result.current.requestRefresh();
+        await act(async () => {
+          await result.current.requestRefresh();
+        });
+
+        expect(result.current.getter.missionItem.title).toBe("Refreshed Mission");
       });
 
-      expect(result.current.getter.missionItem.title).toBe("Refreshed Mission");
+      it("fetchRefreshedResultData 실패 시 에러 메시지를 보여줘야 한다.", async () => {
+        const errorMessage = "Refresh failed";
+        (useRefresh as jest.Mock).mockReturnValue({
+          isRefreshAvailable: true,
+          fetchRefreshedResultData: jest.fn().mockRejectedValue(new Error(errorMessage)),
+        });
+        const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
+
+        const { result } = renderHook(() =>
+          useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
+        );
+
+        await act(async () => {
+          await result.current.requestRefresh();
+        });
+
+        expect(alertMock).toHaveBeenCalledWith(errorMessage);
+        alertMock.mockRestore();
+      });
     });
 
-    it("fetchRefreshedResultData가 에러를 던지면 에러 메시지를 alert으로 표시해야 한다", async () => {
-      const errorMessage = "Refresh failed";
-      (useRefresh as jest.Mock).mockReturnValue({
-        isRefreshAvailable: true,
-        fetchRefreshedResultData: jest.fn().mockRejectedValue(new Error(errorMessage)),
+    describe("requestMissionJudgment 함수 테스트", () => {
+      const mockMission = createMockMission();
+
+      it("fetchJudgmentMissionResult 성공 시 missionItem 상태를 갱신해야 한다", async () => {
+        const { result } = renderHook(() =>
+          useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
+        );
+
+        await act(async () => {
+          await result.current.requestMissionJudgment();
+        });
+
+        expect(result.current.getter.missionItem.title).toBe("Judged Mission");
       });
 
-      const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
+      it("fetchJudgmentMissionResult 실패 시 에러 메시지를 보여줘야 한다.", async () => {
+        const errorMessage = "Judgment failed";
+        (useMissionJudgment as jest.Mock).mockReturnValue({
+          isJudgmentAvailable: true,
+          fetchJudgmentMissionResult: jest.fn().mockRejectedValue(new Error(errorMessage)),
+        });
+        const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
 
-      const { result } = renderHook(() =>
-        useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
-      );
+        const { result } = renderHook(() =>
+          useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
+        );
 
-      await act(async () => {
-        await result.current.requestRefresh();
+        await act(async () => {
+          await result.current.requestMissionJudgment();
+        });
+
+        expect(alertMock).toHaveBeenCalledWith(errorMessage);
+        alertMock.mockRestore();
       });
-
-      expect(alertMock).toHaveBeenCalledWith(errorMessage);
-      alertMock.mockRestore();
-    });
-  });
-
-  describe("requestMissionJudgment 함수", () => {
-    it("fetchJudgmentMissionResult를 호출하고 그 결과로 missionItem을 업데이트해야 한다", async () => {
-      const { result } = renderHook(() =>
-        useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
-      );
-
-      await act(async () => {
-        await result.current.requestMissionJudgment();
-      });
-
-      expect(result.current.getter.missionItem.title).toBe("Judged Mission");
-    });
-
-    it("fetchJudgmentMissionResult가 에러를 던지면 에러 메시지를 alert으로 표시해야 한다", async () => {
-      const errorMessage = "Judgment failed";
-      (useMissionJudgment as jest.Mock).mockReturnValue({
-        isJudgmentAvailable: true,
-        fetchJudgmentMissionResult: jest.fn().mockRejectedValue(new Error(errorMessage)),
-      });
-
-      const alertMock = jest.spyOn(window, "alert").mockImplementation(() => {});
-
-      const { result } = renderHook(() =>
-        useMission({ mission: mockMission, recruitmentId: mockRecruitmentId })
-      );
-
-      await act(async () => {
-        await result.current.requestMissionJudgment();
-      });
-
-      expect(alertMock).toHaveBeenCalledWith(errorMessage);
-      alertMock.mockRestore();
     });
   });
 });
