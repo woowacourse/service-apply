b19400450ce9cfec2929ab3c858c55cb739b03fd
김의진 <gytks4@naver.com>
2025-11-11 17:18:58 +0900

    chore: remove unused import

--- frontend/src/hooks/useAssignmentForm.js
+++ frontend/src/hooks/useAssignmentForm.js
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import { useState } from "react";
 import { ERROR_MESSAGE } from "../constants/messages";
 import { isValidPullRequestUrl } from "../utils/validation/pullRequestUrl";
 import { isValidRepositoryUrl } from "../utils/validation/repositoryUrl";
