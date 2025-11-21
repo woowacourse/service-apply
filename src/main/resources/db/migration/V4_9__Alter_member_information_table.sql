59e36c729361349aa99f5f7771863382213a3577
woowahan-pjs <jason@woowahan.com>
2025-11-21 13:32:10 +0900

    db: remove unnecessary spaces

--- src/main/resources/db/migration/V4_9__Alter_member_information_table.sql
+++ src/main/resources/db/migration/V4_9__Alter_member_information_table.sql
@@ -1,3 +1,3 @@
 alter table member_information
-    modify phone_number varchar (16) not null,
-    modify name varchar (60) not null;
+    modify phone_number varchar(16) not null,
+    modify name varchar(60) not null;
